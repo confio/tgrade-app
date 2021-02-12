15f8bbab5c8a096a56381eceeeaba797a40589cc
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-02-12 17:50:55 +0100

    Fix Token Switch

--- src/App/routes/Cw20Wallet/routes/Token/index.tsx
+++ src/App/routes/Cw20Wallet/routes/Token/index.tsx
@@ -5,26 +5,18 @@ import Allowances from "./routes/Allowances";
 import Detail from "./routes/Detail";
 import Send from "./routes/Send";
 
-const {
-  cw20Wallet: cw20WalletPaths,
-  cw20Wallet: { params: cw20WalletParams },
-} = paths;
-
 export default function Token(): JSX.Element {
   const { path: basePath } = useRouteMatch();
 
   return (
     <Switch>
-      <Route exact path={`${basePath}${cw20WalletParams.contractAddress}${cw20WalletParams.allowingAddress}`}>
+      <Route exact path={basePath}>
         <Detail />
       </Route>
-      <Route
-        exact
-        path={`${basePath}${cw20WalletParams.contractAddress}${cw20WalletParams.allowingAddress}${cw20WalletPaths.send}`}
-      >
+      <Route exact path={`${basePath}${paths.cw20Wallet.send}`}>
         <Send />
       </Route>
-      <Route exact path={`${basePath}${cw20WalletParams.contractAddress}${cw20WalletPaths.allowances}`}>
+      <Route path={`${basePath}${paths.cw20Wallet.allowances}`}>
         <Allowances />
       </Route>
     </Switch>
