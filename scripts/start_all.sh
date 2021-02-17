15e5c307e8ea031fdf03e4c76ae410ea0f4707ff
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-02-17 21:27:29 +0100

    Try to build again

--- scripts/start_all.sh
+++ scripts/start_all.sh
@@ -13,8 +13,14 @@ echo "3 blocks produced!"
 "$SCRIPT_DIR/faucet/start.sh" &
 echo "Wait for faucet to start up..."
 timeout 60 bash -c "until curl -s http://localhost:8000/status > /dev/null; do sleep 0.5; done"
-sleep 3
+sleep 1
 
 "$SCRIPT_DIR/wasmd/init.sh"
 
 echo "All systems go - start your webapp!"
+
+# stay alive forever until the CI kills us
+if [ -n "${CI:-}" ]; then
+  tail -f /dev/null
+  echo "Shutting down!"
+fi
