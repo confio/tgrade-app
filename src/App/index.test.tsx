65df2ac39989d95ec0f18f3d3d5ef40bb985ce41
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-02-17 18:24:41 +0100

    Finish and clean native wallet test

--- src/App/index.test.tsx
+++ src/App/index.test.tsx
@@ -3,7 +3,12 @@ import userEvent from "@testing-library/user-event";
 import * as React from "react";
 import App from "./";
 
-test("should have a happy path for native wallet", async () => {
+describe("should have a happy path for", () => {
+  const emptyAddress = "wasm10tsncqntt778xpkt7d5uwhul2rcmlj5c3larlj";
+
+  test.only("native wallet", async () => {
+    const cosmAmount = "1.5";
+
   render(<App />);
 
   // Login with browser button
@@ -15,24 +20,20 @@ test("should have a happy path for native wallet", async () => {
   const stakeButton = screen.getByRole("button", { name: /stake/i });
   expect(stakeButton).toHaveTextContent(/stake10/i);
 
-  // Go to Cosm detail
+    // Go to Cosm detail with send form
   userEvent.click(cosmButton);
 
   // Enter amount and address
   const sendAmountInput = screen.getByPlaceholderText(/enter amount/i) as HTMLInputElement;
-  await waitFor(() => userEvent.type(sendAmountInput, "1.5", { delay: 1 }));
+    await waitFor(() => userEvent.type(sendAmountInput, cosmAmount, { delay: 1 }));
   const sendAddressInput = screen.getByPlaceholderText(/enter address/i) as HTMLInputElement;
-  await waitFor(() =>
-    userEvent.type(sendAddressInput, "wasm10tsncqntt778xpkt7d5uwhul2rcmlj5c3larlj", { delay: 1 }),
-  );
+    await waitFor(() => userEvent.type(sendAddressInput, emptyAddress, { delay: 1 }));
 
   // Submit and expect success screen
   userEvent.click(screen.getByRole("button", { name: /send/i }));
-  await screen.findByText(
-    "1.5 COSM successfully sent to wasm10tsncqntt778xpkt7d5uwhul2rcmlj5c3larlj",
-    undefined,
-    { timeout: 10_000 },
-  );
+    await screen.findByText(`${cosmAmount} COSM successfully sent to ${emptyAddress}`, undefined, {
+      timeout: 10_000,
+    });
 
   // Go to token list
   userEvent.click(screen.getByRole("button", { name: /tokens/i }));
@@ -44,11 +45,11 @@ test("should have a happy path for native wallet", async () => {
   // Check other account went up
   const searchAddressInput = screen.getByPlaceholderText(/enter address/i) as HTMLInputElement;
   userEvent.clear(searchAddressInput);
-  await waitFor(() =>
-    userEvent.type(searchAddressInput, "wasm10tsncqntt778xpkt7d5uwhul2rcmlj5c3larlj", { delay: 1 }),
-  );
+    await waitFor(() => userEvent.type(searchAddressInput, emptyAddress, { delay: 1 }));
   userEvent.click(screen.getByRole("button", { name: /search/i }));
-  // TODO: find way to wait until cosmjs to that of the other account
-  const otherCosmTokens = (await screen.findByText(/cosm1.5/i)).nextElementSibling;
+    const otherCosmTokens = await screen.findByText(cosmAmount);
+    expect(otherCosmTokens.previousElementSibling).toHaveTextContent(/cosm/i);
+  }, 25_000);
+
   screen.debug();
-}, 10_000);
+});
