06e4236bd2b8648dac2a990146b79a778a76b451
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-02-19 18:07:39 +0100

    Add cw20 wallet test

--- src/App/index.test.tsx
+++ src/App/index.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, waitForElementToBeRemoved } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import * as React from "react";
 import App from "./";
@@ -51,8 +51,102 @@ describe("should have a happy path for", () => {
     expect(otherCosmTokens.previousElementSibling).toHaveTextContent(/cosm/i);
   }, 30_000);
 
-  // TODO: implement this test
-  test.skip("cw20 wallet", () => {});
+  test("cw20 wallet", async () => {
+    const tokenSymbol = "TST";
+    const tokenName = "TST coin";
+    const initialSupply = "1000000000";
+    const mintCapAmount = "2000000000";
+    const myTstBalance = "1000";
+    const mintMoreAmount = "500";
+    const tstAmount = "700";
+
+    render(<App />);
+
+    // Login with browser button and go to cw20 wallet
+    userEvent.click(screen.getByRole("button", { name: /browser/i }));
+    userEvent.click(await screen.findByText(/cw20 wallet/i, undefined, { timeout: 10_000 }));
+
+    // Create new token
+    userEvent.click(await screen.findByText(/create new token/i));
+    const tokenSymbolInput = screen.getByPlaceholderText(/enter symbol/i) as HTMLInputElement;
+    await userEvent.type(tokenSymbolInput, tokenSymbol, { delay: 1 });
+    const tokenNameInput = screen.getByPlaceholderText(/enter token name/i) as HTMLInputElement;
+    await userEvent.type(tokenNameInput, tokenName, { delay: 1 });
+    const decimalsInput = screen.getByPlaceholderText(/select number/i) as HTMLInputElement;
+    await userEvent.type(decimalsInput, "6", { delay: 1 });
+    const initialSupplyInput = screen.getByPlaceholderText(/enter number/i) as HTMLInputElement;
+    await userEvent.type(initialSupplyInput, initialSupply, { delay: 1 });
+    userEvent.click(screen.getByRole("combobox"));
+    userEvent.click(screen.getByText("Fixed cap"));
+    const mintCapInput = screen.getByPlaceholderText(/enter amount/i) as HTMLInputElement;
+    const createButton = screen.getByRole("button", { name: /create/i });
+    await waitFor(
+      async () => {
+        // Clear and write input until mintCap takes value
+        userEvent.clear(mintCapInput);
+        await userEvent.type(mintCapInput, mintCapAmount, { delay: 1 });
+        expect(mintCapInput.value).toBe(mintCapAmount);
+      },
+      { timeout: 10_000 },
+    );
+
+    // Wait input until mintCap validates
+    await waitForElementToBeRemoved(() =>
+      screen.getByText("Cap must be equal or greater than initial supply"),
+    );
+    await waitFor(() => expect(createButton).not.toBeDisabled());
+    userEvent.click(createButton);
+    await screen.findByText(`${initialSupply} ${tokenName} successfully created`, undefined, {
+      timeout: 10_000,
+    });
+
+    // Check balance 1000
+    userEvent.click(screen.getByRole("button", { name: /token detail/i }));
+    await screen.findByText(myTstBalance);
+
+    // Get my account address
+    userEvent.click(screen.getByText(/account/i));
+    const myAddress = screen.getByText(/^wasm/i).textContent || "";
+
+    // Mint 500 to my account
+    userEvent.click(screen.getByText(/cw20 wallet/i));
+    userEvent.click(await screen.findByRole("button", { name: /tst/i }));
+    await screen.findByText(myTstBalance);
+    userEvent.click(await screen.findByRole("button", { name: /mint tokens/i }));
+    const mintMoreInput = screen.getByPlaceholderText(/enter amount/i) as HTMLInputElement;
+    await userEvent.type(mintMoreInput, mintMoreAmount, { delay: 1 });
+    const mintAddressInput = screen.getByPlaceholderText(/enter address/i) as HTMLInputElement;
+    await userEvent.type(mintAddressInput, myAddress, { delay: 1 });
+    userEvent.click(screen.getByRole("button", { name: /mint/i }));
+    await screen.findByText(
+      `${mintMoreAmount} ${tokenSymbol} successfully minted to ${myAddress}`,
+      undefined,
+      {
+        timeout: 10_000,
+      },
+    );
+    userEvent.click(screen.getByRole("button", { name: /token detail/i }));
+    await screen.findByText("1500");
+
+    // Send 700 to another address
+    userEvent.click(screen.getByRole("button", { name: /send/i }));
+
+    // Enter amount and address
+    const sendAmountInput = screen.getByPlaceholderText(/enter amount/i) as HTMLInputElement;
+    await userEvent.type(sendAmountInput, tstAmount, { delay: 1 });
+    const sendAddressInput = screen.getByPlaceholderText(/enter address/i) as HTMLInputElement;
+    await userEvent.type(sendAddressInput, emptyAddress, { delay: 1 });
+
+    // Submit and expect success screen
+    userEvent.click(screen.getByRole("button", { name: /send/i }));
+    await screen.findByText(`${tstAmount} ${tokenSymbol} successfully sent to ${emptyAddress}`, undefined, {
+      timeout: 10_000,
+    });
+
+    // Go to token detail and check my new balance
+    userEvent.click(screen.getByRole("button", { name: /token detail/i }));
+    await screen.findByText("800");
+  }, 75_000);
 
   // TODO: finish this test
   test.skip("staking", async () => {
