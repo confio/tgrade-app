164c5a694bdbc58497ae84887e5555cc39d7b119
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-02-22 18:28:48 +0100

    Tweak styles

--- src/App/components/logic/ErrorAlert/style.ts
+++ src/App/components/logic/ErrorAlert/style.ts
@@ -5,10 +5,25 @@ export const StyledAlert = styled(Alert)`
   &.ant-alert-error {
     background: transparent;
     border: 1px solid var(--color-red);
+    border-radius: 1.25rem;
+
+    img {
+      width: 1.34rem;
+      height: 1.34rem;
+    }
 
     .ant-alert-message,
     .anticon-close {
       color: var(--color-red);
+    }
+
+    .ant-alert-message {
+      margin-left: 0.5rem;
+      text-align: left;
+    }
+
+    .ant-alert-close-icon {
+      align-self: flex-start;
     }
   }
 `;
