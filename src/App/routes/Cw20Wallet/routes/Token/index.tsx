6602fb7efc87740939d6faa94ef00c8060c88b08
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-02-22 18:10:36 +0100

    Fix cw20 routing

--- src/App/routes/Cw20Wallet/routes/Token/index.tsx
+++ src/App/routes/Cw20Wallet/routes/Token/index.tsx
@@ -6,22 +6,27 @@ import Detail from "./routes/Detail";
 import Mint from "./routes/Mint";
 import Send from "./routes/Send";
 
+const {
+  cw20Wallet: cw20WalletPaths,
+  cw20Wallet: { params: cw20WalletParams },
+} = paths;
+
 export default function Token(): JSX.Element {
   const { path: basePath } = useRouteMatch();
 
   return (
     <Switch>
-      <Route exact path={basePath}>
-        <Detail />
-      </Route>
-      <Route exact path={`${basePath}${paths.cw20Wallet.send}`}>
-        <Send />
-      </Route>
-      <Route path={`${basePath}${paths.cw20Wallet.allowances}`}>
+      <Route path={`${basePath}${cw20WalletPaths.allowances}`}>
         <Allowances />
       </Route>
-      <Route path={`${basePath}${paths.cw20Wallet.mint}`}>
+      <Route exact path={`${basePath}${cw20WalletPaths.mint}`}>
         <Mint />
+      </Route>
+      <Route exact path={`${basePath}${cw20WalletParams.allowingAddress}`}>
+        <Detail />
+      </Route>
+      <Route exact path={`${basePath}${cw20WalletParams.allowingAddress}${cw20WalletPaths.send}`}>
+        <Send />
       </Route>
     </Switch>
   );
