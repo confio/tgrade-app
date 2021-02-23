9441d49716e2fbb774309174bd301f437765756a
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-02-23 20:10:26 +0100

    Make deploy_cw20 easily customizable for production

--- scripts/wasmd/deploy_cw20.mjs
+++ scripts/wasmd/deploy_cw20.mjs
@@ -7,14 +7,29 @@ import { Bip39, Random } from "@cosmjs/crypto";
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { FaucetClient } from "@cosmjs/faucet-client";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { GasPrice } from "@cosmjs/launchpad";
 
-const config = {
+const PRODUCTION = false;
+
+const musselnetConfig = {
+  endpoint: "https://rpc.musselnet.cosmwasm.com",
+  faucet: "https://faucet.musselnet.cosmwasm.com",
+  bech32prefix: "wasm",
+  feeDenom: "umayo",
+  gasPrice: GasPrice.fromString("0.025umayo"),
+  wasmUrl: "https://github.com/CosmWasm/cosmwasm-plus/releases/download/v0.5.0/cw20_base.wasm",
+};
+
+const localConfig = {
   endpoint: "http://localhost:26657",
   faucet: "http://localhost:8000",
   bech32prefix: "wasm",
   feeDenom: "ucosm",
+  gasPrice: GasPrice.fromString("0.025ucosm"),
   wasmUrl: "https://github.com/CosmWasm/cosmwasm-plus/releases/download/v0.5.0/cw20_base.wasm",
 };
+
+const config = PRODUCTION ? musselnetConfig : localConfig;
 
 const codeMeta = {
   source: "https://github.com/CosmWasm/cosmwasm-plus/tree/v0.5.0/contracts/cw20-base",
@@ -78,7 +93,8 @@ async function main() {
   // build signing client
   const mnemonic = Bip39.encode(Random.getBytes(16)).toString();
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, undefined, config.bech32prefix);
-  const client = await SigningCosmWasmClient.connectWithSigner(config.endpoint, wallet);
+  const options = { prefix: config.bech32prefix, gasPrice: config.gasPrice };
+  const client = await SigningCosmWasmClient.connectWithSigner(config.endpoint, wallet, options);
 
   // get fee tokens
   console.info("Hitting the faucet...");
