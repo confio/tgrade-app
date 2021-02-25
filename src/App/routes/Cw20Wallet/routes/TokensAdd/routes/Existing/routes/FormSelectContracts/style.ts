0cf772141f5baf0c1a19adda04333cdb1cb9cbfb
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-02-25 16:58:02 +0100

    Remove unneeded styles for forms

--- src/App/routes/Cw20Wallet/routes/TokensAdd/routes/Existing/routes/FormSelectContracts/style.ts
+++ src/App/routes/Cw20Wallet/routes/TokensAdd/routes/Existing/routes/FormSelectContracts/style.ts
@@ -3,16 +3,13 @@ import styled from "styled-components";
 
 export const FormStack = styled(Stack)`
   & > * {
-    --gap: var(--s4);
+    --gap: var(--s2);
   }
 
   & .ant-form-item .ant-form-item-control-input-content {
     &:hover {
       color: #43547d;
       border: 1px solid #43547d;
-  }
-
-  button {
-    margin-top: var(--s2);
+    }
   }
 `;
