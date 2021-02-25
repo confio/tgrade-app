ecce127a63aeb5961e16d5ebd38e6461e18c36d1
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-02-25 11:50:48 +0100

    Improve getErrorFromStackTrace

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,8 +1,7 @@
 // Get string error from operation stack trace. Matches last line but removes last "message index" part
 export function getErrorFromStackTrace(stackTrace: Error): string {
   const stringStackTrace = stackTrace.message || stackTrace.stack || stackTrace.toString() || "";
-  const match = stringStackTrace.match(/.*\s*$/g)?.[0];
-  const error = match?.substring(0, match.lastIndexOf(";"));
-
+  // matches everything after the last ";"
+  const error = stringStackTrace.match(/([^;]*)$/)?.[0]?.trimStart();
   return error || stringStackTrace;
 }
