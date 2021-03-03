abd8a8689056f2fa937677dda68ff70a5e3ac055
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-03-03 08:52:26 +0100

    Tweak Stack with spacings

--- src/App/components/layout/Stack/component.tsx
+++ src/App/components/layout/Stack/component.tsx
@@ -1,8 +1,10 @@
 import * as React from "react";
 import { HTMLAttributes } from "react";
+import { Spacing } from "theme/GlobalStyle/GlobalSpacing";
 
 interface StackProps extends HTMLAttributes<HTMLOrSVGElement> {
   readonly tag?: keyof JSX.IntrinsicElements;
+  readonly gap?: Spacing;
 }
 
 export default function Stack({ tag: Tag = "div", children, ...props }: StackProps): JSX.Element {
