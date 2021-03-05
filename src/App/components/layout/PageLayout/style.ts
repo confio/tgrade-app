544a89e759593073be57e254afd7d53e2773c82c
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-03-05 09:41:11 +0100

    Add styles for new Menu

--- src/App/components/layout/PageLayout/style.ts
+++ src/App/components/layout/PageLayout/style.ts
@@ -17,16 +17,55 @@ export const StyledPageLayout = styled(PageLayout)`
   }
 `;
 
+export const MenuWrapper = styled.div`
+  /* Position and sizing of burger button */
+  & .bm-burger-button {
+    display: none;
+  }
+
+  /* Color/shape of close button cross */
+  & .bm-cross {
+    background: white;
+  }
+
+  /* General sidebar styles */
+  & .bm-menu {
+    background: var(--grad-primary);
+    padding: 2rem;
+  }
+
+  &[data-background-big="true"] .bm-menu {
+    background: var(--color-form-focus);
+  }
+
+  /* Wrapper for item list */
+  & .bm-item-list {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+
+  /* Individual item */
+  & .bm-item {
+    display: inline-block;
+
+    h3.ant-typography {
+      color: white;
+      margin: 0;
+    }
+  }
+`;
+
 export const NavHeader = styled.nav`
   display: flex;
   align-items: center;
   justify-content: space-between;
 
-  [alt="Back arrow"] {
+  & [alt="Back arrow"] {
     margin-right: auto;
   }
 
-  [aria-label="Toggle menu"] {
+  & [aria-label="Toggle menu"] {
     margin-left: auto;
   }
 `;
