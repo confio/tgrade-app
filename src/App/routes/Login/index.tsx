fb12789c1b5a1b9d76bc6f0bab3ba1a74067a049
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-03-08 15:29:37 +0100

    Adapt Login to LayoutProvider

--- src/App/routes/Login/index.tsx
+++ src/App/routes/Login/index.tsx
@@ -1,15 +1,15 @@
 import { Button, Typography } from "antd";
-import { PageLayout, Stack } from "App/components/layout";
-import { Loading } from "App/components/logic";
-import { RedirectLocation } from "App/components/logic/ProtectedSwitch";
-import { paths } from "App/paths";
+import { Stack } from "App/components/layout";
+//import { RedirectLocation } from "App/components/logic";
+//import { paths } from "App/paths";
 import { configKeplr } from "config/keplr";
 import * as React from "react";
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import { isChrome, isDesktop } from "react-device-detect";
 import { useTranslation } from "react-i18next";
-import { useHistory } from "react-router-dom";
+//import { useHistory } from "react-router-dom";
 import { useError, useSdk } from "service";
+import { useLayout } from "service/layout";
 import { loadKeplrWallet, loadLedgerWallet, loadOrCreateWallet, WalletLoader } from "utils/sdk";
 import cosmWasmLogo from "./assets/cosmWasmLogo.svg";
 import { LightText, Logo } from "./style";
@@ -28,23 +28,23 @@ function disableKeplrLogin() {
 
 export default function Login(): JSX.Element {
   const { t } = useTranslation("login");
-  const history = useHistory();
-  const state = history.location.state as RedirectLocation;
+  //const history = useHistory();
+  //const state = history.location.state as RedirectLocation;
+  const { setLoading } = useLayout({ hideMenu: true });
+
   const { handleError } = useError();
   const sdk = useSdk();
   const config = sdk.getConfig();
 
-  const [initializing, setInitializing] = useState(false);
-
   async function init(loadWallet: WalletLoader) {
-    setInitializing(true);
+    setLoading("Initializing app...");
 
     try {
       const signer = await loadWallet(config.chainId, config.addressPrefix);
       sdk.init(signer);
     } catch (error) {
-      setInitializing(false);
       handleError(error);
+      setLoading(false);
     }
   }
 
@@ -67,7 +67,7 @@ export default function Login(): JSX.Element {
     }
   }
 
-  useEffect(() => {
+  /* useEffect(() => {
     if (!sdk.initialized) return;
 
     if (state) {
@@ -75,33 +75,29 @@ export default function Login(): JSX.Element {
     } else {
       history.push(paths.wallet.prefix);
     }
-  }, [history, sdk.initialized, state]);
+  }, [history, sdk.initialized, state]); */
 
-  return initializing ? (
-    <Loading loadingText="Initializing app..." />
-  ) : (
-    <PageLayout hide="header">
-      <Stack gap="s5">
-        <Logo src={cosmWasmLogo} alt="CosmWasm logo" />
-        <Stack gap="s3">
-          <Stack gap="s-1">
-            <Title level={1}>{t("hello")}</Title>
-            <LightText>{t("welcome")}</LightText>
-            <LightText>{t("select")}</LightText>
-          </Stack>
-          <Stack>
-            <Button data-size="large" type="primary" onClick={initBrowser}>
-              {t("browserButton")}
-            </Button>
-            <Button data-size="large" type="primary" disabled={disableLedgerLogin()} onClick={initLedger}>
-              {t("ledgerButton")}
-            </Button>
-            <Button data-size="large" type="primary" disabled={disableKeplrLogin()} onClick={initKeplr}>
-              {t("keplrButton")}
-            </Button>
-          </Stack>
+  return (
+    <Stack gap="s5">
+      <Logo src={cosmWasmLogo} alt="CosmWasm logo" />
+      <Stack gap="s3">
+        <Stack gap="s-1">
+          <Title level={1}>{t("hello")}</Title>
+          <LightText>{t("welcome")}</LightText>
+          <LightText>{t("select")}</LightText>
+        </Stack>
+        <Stack>
+          <Button data-size="large" type="primary" onClick={initBrowser}>
+            {t("browserButton")}
+          </Button>
+          <Button data-size="large" type="primary" disabled={disableLedgerLogin()} onClick={initLedger}>
+            {t("ledgerButton")}
+          </Button>
+          <Button data-size="large" type="primary" disabled={disableKeplrLogin()} onClick={initKeplr}>
+            {t("keplrButton")}
+          </Button>
         </Stack>
       </Stack>
-    </PageLayout>
+    </Stack>
   );
 }
