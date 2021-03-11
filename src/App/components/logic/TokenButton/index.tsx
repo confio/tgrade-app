e6b18781e8617e2b8726808ebdac5b6865763c27
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-03-11 16:55:50 +0100

    Remove unneeded Tokens label

--- src/App/components/logic/TokenButton/index.tsx
+++ src/App/components/logic/TokenButton/index.tsx
@@ -1,7 +1,6 @@
 import { Button, Typography } from "antd";
 import * as React from "react";
 import { ComponentProps } from "react";
-import { useTranslation } from "react-i18next";
 import { TokenItem } from "./style";
 
 const { Text } = Typography;
@@ -12,12 +11,11 @@ interface TokenButtonProps extends ComponentProps<typeof Button> {
 }
 
 export default function TokenButton({ denom, amount, ...restProps }: TokenButtonProps): JSX.Element {
-  const { t } = useTranslation("wallet");
   return (
     <Button data-size="large" type="primary" {...restProps}>
       <TokenItem>
         <Text>{denom}</Text>
-        <Text>{`${amount} ${t("tokens")}`}</Text>
+        <Text>{amount}</Text>
       </TokenItem>
     </Button>
   );
