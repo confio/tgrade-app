a09199ae76bc3ffa116b4a894d115df7dce7576b
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-03-11 16:52:09 +0100

    Show 0 tokens

--- src/App/components/logic/TokenButton/index.tsx
+++ src/App/components/logic/TokenButton/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Typography } from "antd";
 import * as React from "react";
 import { ComponentProps } from "react";
+import { useTranslation } from "react-i18next";
 import { TokenItem } from "./style";
 
 const { Text } = Typography;
@@ -11,11 +12,12 @@ interface TokenButtonProps extends ComponentProps<typeof Button> {
 }
 
 export default function TokenButton({ denom, amount, ...restProps }: TokenButtonProps): JSX.Element {
+  const { t } = useTranslation("wallet");
   return (
     <Button data-size="large" type="primary" {...restProps}>
       <TokenItem>
         <Text>{denom}</Text>
-        <Text>{amount !== "0" ? amount : "No tokens"}</Text>
+        <Text>{`${amount} ${t("tokens")}`}</Text>
       </TokenItem>
     </Button>
   );
