73a1aba37aa647d0bb54edd87954d7f7a3a5a2d1
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-03-16 09:19:00 +0100

    Fix disabled native token for empty balance

--- src/App/routes/Wallet/routes/Tokens/components/TokenList.tsx
+++ src/App/routes/Wallet/routes/Tokens/components/TokenList.tsx
@@ -65,9 +65,8 @@ export default function TokenList({ currentAddress }: TokenListProps): JSX.Eleme
             key={nativeToken.denom}
             denom={denomToDisplay}
             amount={amountToDisplay}
-            onClick={() => {
-              amAllowed && goTokenDetail(nativeToken);
-            }}
+            disabled={amountToDisplay === "0"}
+            onClick={() => goTokenDetail(nativeToken)}
           />
         );
       })}
