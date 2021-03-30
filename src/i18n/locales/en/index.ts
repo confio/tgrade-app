5eecfc4db09ef04822e3947ec83d005483d1e184
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-03-30 20:56:39 +0200

    Add locales

--- src/i18n/locales/en/index.ts
+++ src/i18n/locales/en/index.ts
@@ -1,7 +1,8 @@
+import account from "./account.json";
 import common from "./common.json";
 import cw20Wallet from "./cw20Wallet.json";
 import login from "./login.json";
 import staking from "./staking.json";
 import wallet from "./wallet.json";
 
-export const en = { common, login, wallet, cw20Wallet, staking };
+export const en = { common, login, account, wallet, cw20Wallet, staking };
--- src/i18n/locales/ru/index.ts
+++ src/i18n/locales/ru/index.ts
@@ -1,7 +1,8 @@
+import account from "./account.json";
 import common from "./common.json";
 import cw20Wallet from "./cw20Wallet.json";
 import login from "./login.json";
 import staking from "./staking.json";
 import wallet from "./wallet.json";
 
-export const ru = { common, login, wallet, cw20Wallet, staking };
+export const ru = { common, login, account, wallet, cw20Wallet, staking };
