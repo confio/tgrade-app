f0981d695f5083f0fbf9ccd6b6e07505061e42bf
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-04-28 10:42:11 +0200

    Add initial react-query support to CW20 wallet

--- src/App/routes/Cw20Wallet/routes/TokensAdd/routes/Existing/routes/FormInputContract.tsx
+++ src/App/routes/Cw20Wallet/routes/TokensAdd/routes/Existing/routes/FormInputContract.tsx
@@ -6,12 +6,17 @@ import { Formik } from "formik";
 import { Form, FormItem, Input } from "formik-antd";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
+import { useMutation } from "react-query";
 import { useHistory, useRouteMatch } from "react-router-dom";
 import { useContracts, useError, useSdk } from "service";
-import { CW20 } from "utils/cw20";
+import { CW20, CW20Instance } from "utils/cw20";
 import { getErrorFromStackTrace } from "utils/errors";
 import { getAddressField } from "utils/forms";
 import * as Yup from "yup";
+
+interface MutateVariables {
+  readonly newCw20Contract: CW20Instance;
+}
 
 interface FormInputContractFields {
   readonly codeIdOrAddress: string;
@@ -27,31 +32,14 @@ export default function FormInputContract(): JSX.Element {
   } = useSdk();
   const { addContract } = useContracts();
 
-  async function submitInputContract({ codeIdOrAddress }: FormInputContractFields) {
-    const codeId = Number.parseInt(codeIdOrAddress, 10);
-    if (!Number.isNaN(codeId)) {
-      history.push(`${pathExisting}/${codeId}`);
-      return;
-    }
+  async function mutationFn({ newCw20Contract }: MutateVariables) {
+    // If the new contract has no tokenInfo, it throws
+    await newCw20Contract.tokenInfo();
+  }
 
-    const contractAddress = codeIdOrAddress;
-    const newCw20Contract = CW20(signingClient).use(contractAddress);
-
-    try {
-      // If the new contract has no tokenInfo, it throws
-      await newCw20Contract.tokenInfo();
-      addContract(newCw20Contract);
-
-      history.push({
-        pathname: paths.operationResult,
-        state: {
-          success: true,
-          message: t("cw20Wallet:addContractSuccess", { contractAddress }),
-          customButtonText: t("cw20Wallet:tokens"),
-          customButtonActionPath: `${paths.cw20Wallet.prefix}${paths.cw20Wallet.tokens}`,
-        } as OperationResultState,
-      });
-    } catch (stackTrace) {
+  const mutationOptions = {
+    mutationKey: "addCodeId",
+    onError: (stackTrace: Error) => {
       handleError(stackTrace);
 
       history.push({
@@ -63,7 +51,34 @@ export default function FormInputContract(): JSX.Element {
           customButtonActionPath: pathExisting,
         } as OperationResultState,
       });
+    },
+    onSuccess: (_: unknown, { newCw20Contract }: MutateVariables) => {
+      addContract(newCw20Contract);
+
+      history.push({
+        pathname: paths.operationResult,
+        state: {
+          success: true,
+          message: t("cw20Wallet:addContractSuccess", { contractAddress: newCw20Contract.contractAddress }),
+          customButtonText: t("cw20Wallet:tokens"),
+          customButtonActionPath: `${paths.cw20Wallet.prefix}${paths.cw20Wallet.tokens}`,
+        } as OperationResultState,
+      });
+    },
+  };
+
+  const { mutate } = useMutation(mutationFn, mutationOptions);
+
+  function submitInputContract({ codeIdOrAddress }: FormInputContractFields) {
+    const codeId = Number.parseInt(codeIdOrAddress, 10);
+    if (!Number.isNaN(codeId)) {
+      history.push(`${pathExisting}/${codeId}`);
+      return;
     }
+
+    const contractAddress = codeIdOrAddress;
+    const newCw20Contract = CW20(signingClient).use(contractAddress);
+    mutate({ newCw20Contract });
   }
 
   const validationSchema = Yup.object().shape({
