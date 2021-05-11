45e6d9799c668ec5eccdf7467b020799b8ecf7ab
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-05-11 16:52:42 +0200

    Add CorporateBanner and Titles to layout service

--- src/service/layout.tsx
+++ src/service/layout.tsx
@@ -10,14 +10,27 @@ import { useWindowSize } from "utils/ui";
 
 type MenuState = "open" | "closed" | "hidden";
 
+type ViewTitles = {
+  readonly viewTitle?: string;
+  readonly viewSubtitle?: string;
+};
+
 type LayoutAction =
   | {
       readonly type: "setMenu";
       readonly payload: MenuState;
     }
   | {
+      readonly type: "setCorporateBanner";
+      readonly payload: boolean;
+    }
+  | {
       readonly type: "setBackButtonProps";
       readonly payload?: ComponentProps<typeof BackButton>;
+    }
+  | {
+      readonly type: "setTitles";
+      readonly payload?: ViewTitles;
     }
   | {
       readonly type: "setLoading";
@@ -27,7 +40,9 @@ type LayoutAction =
 type LayoutDispatch = (action: LayoutAction) => void;
 type LayoutState = {
   readonly menuState: MenuState;
+  readonly showCorporateBanner: boolean;
   readonly backButtonProps?: ComponentProps<typeof BackButton>;
+  readonly viewTitles?: ViewTitles;
   readonly isLoading: boolean;
   readonly loadingMsg?: string;
 };
@@ -46,8 +61,14 @@ function layoutReducer(state: LayoutState, action: LayoutAction): LayoutState {
     case "setMenu": {
       return { ...state, menuState: action.payload };
     }
+    case "setCorporateBanner": {
+      return { ...state, showCorporateBanner: action.payload };
+    }
     case "setBackButtonProps": {
       return { ...state, backButtonProps: action.payload };
+    }
+    case "setTitles": {
+      return { ...state, viewTitles: action.payload };
     }
     case "setLoading": {
       if (typeof action.payload === "boolean") {
@@ -67,11 +88,19 @@ export const openMenu = (dispatch: LayoutDispatch): void => dispatch({ type: "se
 export const closeMenu = (dispatch: LayoutDispatch): void => dispatch({ type: "setMenu", payload: "closed" });
 export const showMenu = closeMenu;
 
+export function setCorporateBanner(dispatch: LayoutDispatch, showCorporateBanner: boolean): void {
+  dispatch({ type: "setCorporateBanner", payload: showCorporateBanner });
+}
+
 export function setBackButtonProps(
   dispatch: LayoutDispatch,
   backButtonProps?: ComponentProps<typeof BackButton>,
 ): void {
   dispatch({ type: "setBackButtonProps", payload: backButtonProps });
+}
+
+export function setTitles(dispatch: LayoutDispatch, viewTitles?: ViewTitles): void {
+  dispatch({ type: "setTitles", payload: viewTitles });
 }
 
 export function setLoading(dispatch: LayoutDispatch, loading: boolean | string): void {
@@ -83,8 +112,10 @@ type InitialLayoutState = {
 };
 
 export function setInitialLayoutState(dispatch: LayoutDispatch, state?: InitialLayoutState): void {
-  const { backButtonProps, menuState } = state || { backButtonProps: undefined, menuState: undefined };
+  const { backButtonProps, viewTitles, menuState, showCorporateBanner } = state ?? {};
   setBackButtonProps(dispatch, backButtonProps);
+  setCorporateBanner(dispatch, !!showCorporateBanner);
+  setTitles(dispatch, viewTitles);
 
   switch (menuState) {
     case "open": {
@@ -116,7 +147,11 @@ export default function LayoutProvider({ children }: HTMLAttributes<HTMLOrSVGEle
   const history = useHistory();
   const state = history.location.state as RedirectLocation;
   const { sdkState } = useSdkInit();
-  const [layoutState, layoutDispatch] = useReducer(layoutReducer, { menuState: "hidden", isLoading: false });
+  const [layoutState, layoutDispatch] = useReducer(layoutReducer, {
+    menuState: "hidden",
+    showCorporateBanner: true,
+    isLoading: false,
+  });
 
   useEffect(() => {
     (async function loginIfInitialized() {
@@ -136,7 +171,6 @@ export default function LayoutProvider({ children }: HTMLAttributes<HTMLOrSVGEle
 
   const { width } = useWindowSize();
   const showMenu = layoutState.menuState !== "hidden" && !layoutState.isLoading;
-  const showCorporateBanner = true;
 
   return (
     <LayoutContext.Provider value={{ layoutState, layoutDispatch }}>
@@ -148,7 +182,11 @@ export default function LayoutProvider({ children }: HTMLAttributes<HTMLOrSVGEle
             closeMenu={() => closeMenu(layoutDispatch)}
           />
         ) : null}
-        {showCorporateBanner ? <CorporateBannerLayout>{children}</CorporateBannerLayout> : children}
+        {layoutState.showCorporateBanner ? (
+          <CorporateBannerLayout>{children}</CorporateBannerLayout>
+        ) : (
+          children
+        )}
       </>
     </LayoutContext.Provider>
   );
