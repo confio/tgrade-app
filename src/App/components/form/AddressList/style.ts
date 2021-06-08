3c05d70d633303952b4a9b72037367c543ce1b48
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-06-08 11:09:49 +0200

    Add AddressList form component

---
+++ src/App/components/form/AddressList/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const AddressesContainer = styled.div`
+  border: 1px solid var(--color-input-border);
+  border-radius: 6px;
+  padding: var(--s-1);
+  background-color: white;
+`;
