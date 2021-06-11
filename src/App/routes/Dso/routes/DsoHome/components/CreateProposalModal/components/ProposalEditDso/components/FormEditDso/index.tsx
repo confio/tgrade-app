10da989cf7a46d8c8a5572e10ffa5f1153b7d944
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-06-11 16:59:25 +0200

    Add current values to DSO validation

--- src/App/routes/Dso/routes/DsoHome/components/CreateProposalModal/components/ProposalEditDso/components/FormEditDso/index.tsx
+++ src/App/routes/Dso/routes/DsoHome/components/CreateProposalModal/components/ProposalEditDso/components/FormEditDso/index.tsx
@@ -2,17 +2,38 @@ import Button from "App/components/form/Button";
 import Checkbox from "App/components/form/Checkbox";
 import { Field } from "App/components/form/Field";
 import { BackButtonOrLink } from "App/components/logic";
-import { DsoHomeParams } from "App/routes/Dso/routes/DsoHome";
 import { Formik } from "formik";
 import { Form } from "formik-antd";
 import * as React from "react";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { useError, useSdk } from "service";
-import { nativeCoinToDisplay } from "utils/currency";
 import { getFormItemName } from "utils/forms";
 import * as Yup from "yup";
 import { ButtonGroup, FieldGroup, FormStack, Separator } from "./style";
+
+function getFormValuesWithType(values: { [x: string]: string | boolean }): FormEditDsoValues {
+  return {
+    dsoName: values[getFormItemName(dsoNameLabel)].toString(),
+    quorum: values[getFormItemName(quorumLabel)].toString(),
+    threshold: values[getFormItemName(thresholdLabel)].toString(),
+    votingDuration: values[getFormItemName(votingDurationLabel)].toString(),
+    escrowAmount: values[getFormItemName(escrowAmountLabel)].toString(),
+    earlyPass: !!values[getFormItemName(earlyPassLabel)],
+    comment: values[getFormItemName(commentLabel)].toString(),
+  };
+}
+
+function checkEditParamsAreNew(
+  formValues: FormEditDsoValues,
+  currentValues: Omit<FormEditDsoValues, "comment">,
+): boolean {
+  return (
+    formValues.dsoName !== currentValues.dsoName ||
+    formValues.quorum !== currentValues.quorum ||
+    formValues.threshold !== currentValues.threshold ||
+    formValues.votingDuration !== currentValues.votingDuration ||
+    formValues.escrowAmount !== currentValues.escrowAmount ||
+    formValues.earlyPass !== currentValues.earlyPass
+  );
+}
 
 const dsoNameLabel = "Add new name for the DSO";
 const quorumLabel = "Quorum";
@@ -54,11 +75,13 @@ export interface FormEditDsoValues {
 interface FormEditDsoProps extends FormEditDsoValues {
   readonly goBack: () => void;
   readonly handleSubmit: (values: FormEditDsoValues) => void;
+  readonly currentDsoValues: Omit<FormEditDsoValues, "comment">;
 }
 
 export default function FormEditDso({
   goBack,
   handleSubmit,
+  currentDsoValues,
   dsoName,
   quorum,
   threshold,
@@ -67,38 +90,6 @@ export default function FormEditDso({
   earlyPass,
   comment,
 }: FormEditDsoProps): JSX.Element {
-  const { dsoAddress }: DsoHomeParams = useParams();
-  const { handleError } = useError();
-  const {
-    sdkState: { signingClient, config },
-  } = useSdk();
-
-  const [dsoNamePlaceholder, setDsoNamePlaceholder] = useState("");
-  const [quorumPlaceholder, setQuorumPlaceholder] = useState("");
-  const [thresholdPlaceholder, setThresholdPlaceholder] = useState("");
-  const [votingDurationPlaceholder, setVotingDurationPlaceholder] = useState("");
-  const [escrowAmountPlaceholder, setEscrowAmountPlaceholder] = useState("");
-
-  useEffect(() => {
-    (async function queryDso() {
-      try {
-        const dsoQuery = await signingClient.queryContractSmart(dsoAddress, { dso: {} });
-        const escrowCoin = nativeCoinToDisplay(
-          { denom: config.feeToken, amount: dsoQuery.escrow_amount },
-          config.coinMap,
-        );
-
-        setDsoNamePlaceholder(dsoQuery.name);
-        setQuorumPlaceholder((parseFloat(dsoQuery.rules.quorum) * 100).toString());
-        setThresholdPlaceholder((parseFloat(dsoQuery.rules.threshold) * 100).toString());
-        setVotingDurationPlaceholder(dsoQuery.rules.voting_period);
-        setEscrowAmountPlaceholder(escrowCoin.amount);
-      } catch (error) {
-        handleError(error);
-      }
-    })();
-  }, [config.coinMap, config.feeToken, dsoAddress, handleError, signingClient]);
-
   return (
     <Formik
       initialValues={{
@@ -110,50 +101,34 @@ export default function FormEditDso({
         [getFormItemName(earlyPassLabel)]: earlyPass,
         [getFormItemName(commentLabel)]: comment,
       }}
+      enableReinitialize
       validationSchema={validationSchema}
-      onSubmit={(values) =>
-        handleSubmit({
-          dsoName: values[getFormItemName(dsoNameLabel)].toString(),
-          quorum: values[getFormItemName(quorumLabel)].toString(),
-          threshold: values[getFormItemName(thresholdLabel)].toString(),
-          votingDuration: values[getFormItemName(votingDurationLabel)].toString(),
-          escrowAmount: values[getFormItemName(escrowAmountLabel)].toString(),
-          earlyPass: !!values[getFormItemName(earlyPassLabel)],
-          comment: values[getFormItemName(commentLabel)].toString(),
-        })
-      }
+      onSubmit={(values) => handleSubmit(getFormValuesWithType(values))}
     >
-      {({ dirty, isValid, submitForm }) => (
+      {({ values, isValid, submitForm }) => (
         <>
           <Form>
             <FormStack gap="s1">
-              <Field label={dsoNameLabel} placeholder={`${dsoNamePlaceholder || "Enter DSO name"}`} />
+              <Field label={dsoNameLabel} placeholder="Enter DSO name" />
               <FieldGroup>
-                <Field label={quorumLabel} placeholder={`${quorumPlaceholder || "Enter quorum"}`} units="%" />
-                <Field
-                  label={thresholdLabel}
-                  placeholder={`${thresholdPlaceholder || "Enter threshold"}`}
-                  units="%"
-                />
+                <Field label={quorumLabel} placeholder="Enter quorum" units="%" />
+                <Field label={thresholdLabel} placeholder="Enter threshold" units="%" />
               </FieldGroup>
               <FieldGroup>
-                <Field
-                  label={votingDurationLabel}
-                  placeholder={`${votingDurationPlaceholder || "Enter duration"}`}
-                  units="Days"
-                />
-                <Field
-                  label={escrowAmountLabel}
-                  placeholder={`${escrowAmountPlaceholder || "Enter amount"}`}
-                  units="TGD"
-                />
+                <Field label={votingDurationLabel} placeholder="Enter duration" units="Days" />
+                <Field label={escrowAmountLabel} placeholder="Enter amount" units="TGD" />
               </FieldGroup>
               <Checkbox name={getFormItemName(earlyPassLabel)}>{earlyPassLabel}</Checkbox>
               <Field label={commentLabel} placeholder="Enter comment" />
               <Separator />
               <ButtonGroup>
                 <BackButtonOrLink onClick={() => goBack()} text="Back" />
-                <Button disabled={!isValid} onClick={() => submitForm()}>
+                <Button
+                  disabled={
+                    !isValid || !checkEditParamsAreNew(getFormValuesWithType(values), currentDsoValues)
+                  }
+                  onClick={() => submitForm()}
+                >
                   <div>Next</div>
                 </Button>
               </ButtonGroup>
