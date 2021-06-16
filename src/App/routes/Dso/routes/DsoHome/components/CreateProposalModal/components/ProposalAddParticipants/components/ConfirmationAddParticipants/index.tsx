9594275c93e3ee527cf0c1713844585293215e17
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-06-16 12:03:31 +0200

    Add tx fee to AddParticipants

--- src/App/routes/Dso/routes/DsoHome/components/CreateProposalModal/components/ProposalAddParticipants/components/ConfirmationAddParticipants/index.tsx
+++ src/App/routes/Dso/routes/DsoHome/components/CreateProposalModal/components/ProposalAddParticipants/components/ConfirmationAddParticipants/index.tsx
@@ -3,10 +3,12 @@ import { AddressList } from "App/components/form/AddressList";
 import Button from "App/components/form/Button";
 import { BackButtonOrLink } from "App/components/logic";
 import * as React from "react";
-import { useSdk } from "service";
-import { AddressStack, ButtonGroup, Separator, TextComment } from "./style";
+import { useEffect, useState } from "react";
+import { useError, useSdk } from "service";
+import { getDisplayAmountFromFee } from "utils/currency";
+import { AddressStack, ButtonGroup, Separator, TextComment, FeeGroup } from "./style";
 
-const { Text } = Typography;
+const { Text, Paragraph } = Typography;
 
 interface ConfirmationAddParticipantsProps {
   readonly members: readonly string[];
@@ -23,25 +25,42 @@ export default function ConfirmationAddParticipants({
   goBack,
   submitForm,
 }: ConfirmationAddParticipantsProps): JSX.Element {
+  const { handleError } = useError();
   const {
-    sdkState: {
-      config: { addressPrefix },
-    },
+    sdkState: { config, signingClient },
   } = useSdk();
+
+  const [txFee, setTxFee] = useState("0");
+  const feeTokenDenom = config.coinMap[config.feeToken].denom || "";
+
+  useEffect(() => {
+    try {
+      const txFee = getDisplayAmountFromFee(signingClient.fees.exec, config);
+      setTxFee(txFee);
+    } catch (error) {
+      handleError(error);
+    }
+  }, [config, handleError, signingClient.fees.exec]);
 
   return (
     <>
       <AddressStack gap="s-3">
         <Text>Non voting participant(s) to be added</Text>
-        <AddressList addresses={members} addressPrefix={addressPrefix} />
+        <AddressList addresses={members} addressPrefix={config.addressPrefix} />
       </AddressStack>
       <TextComment>{comment}</TextComment>
       <Separator />
       <ButtonGroup>
         <BackButtonOrLink disabled={isSubmitting} onClick={() => goBack()} text="Back" />
-        <Button loading={isSubmitting} onClick={() => submitForm()}>
-          <div>Confirm proposal</div>
-        </Button>
+        <FeeGroup>
+          <Typography>
+            <Paragraph>Tx fee</Paragraph>
+            <Paragraph>{`~${txFee} ${feeTokenDenom}`}</Paragraph>
+          </Typography>
+          <Button loading={isSubmitting} onClick={() => submitForm()}>
+            <div>Confirm proposal</div>
+          </Button>
+        </FeeGroup>
       </ButtonGroup>
     </>
   );
