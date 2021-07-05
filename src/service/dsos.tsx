c97bedff840e75586a906fab36f75192aa2cc43a
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-07-05 12:26:03 +0200

    Improve DsoProvider so it stores addresses

--- src/service/dsos.tsx
+++ src/service/dsos.tsx
@@ -1,4 +1,5 @@
 import { AddDsoModal } from "App/components/logic";
+import LeaveDsoModal from "App/components/logic/LeaveDsoModal";
 import * as React from "react";
 import { createContext, HTMLAttributes, useContext, useEffect, useReducer } from "react";
 import { useSdk } from "service";
@@ -9,28 +10,42 @@ interface DsoId {
   readonly name: string;
 }
 
-type DsosByUserMap = Map<string, readonly DsoId[]>;
+type DsoAddressesByUserMap = Map<string, readonly string[]>;
 
 type ModalState = "open" | "closed";
 
 type DsoAction =
   | {
       readonly type: "addDso";
-      readonly payload: DsoId;
+      readonly payload: string;
     }
   | {
       readonly type: "removeDso";
       readonly payload: string;
     }
   | {
+      readonly type: "setDsoAddresses";
+      readonly payload: readonly string[];
+    }
+  | {
+      readonly type: "setDsoIds";
+      readonly payload: readonly DsoId[];
+    }
+  | {
       readonly type: "setAddDsoModal";
+      readonly payload: ModalState;
+    }
+  | {
+      readonly type: "setLeaveDsoModal";
       readonly payload: ModalState;
     };
 
 type DsoDispatch = (action: DsoAction) => void;
 type DsoState = {
+  readonly dsoAddresses: readonly string[];
   readonly dsos: readonly DsoId[];
   readonly addDsoModalState: ModalState;
+  readonly leaveDsoModalState: ModalState;
 };
 
 type DsoContextType =
@@ -53,26 +68,25 @@ function dsoComparator({ name: nameA }: DsoId, { name: nameB }: DsoId) {
 const DsoContext = createContext<DsoContextType>(undefined);
 
 function dsoReducer(dsoState: DsoState, action: DsoAction): DsoState {
-  const prevDsos = dsoState.dsos;
-
   switch (action.type) {
     case "addDso": {
-      const newDso = action.payload;
-      const dsosRemovedOld = prevDsos.filter(({ address }) => address !== newDso.address);
-      const newDsos = [...dsosRemovedOld, newDso];
-      const sortedDsos = newDsos.sort(dsoComparator);
-
-      return { ...dsoState, dsos: sortedDsos };
+      return { ...dsoState, dsoAddresses: [...new Set([...dsoState.dsoAddresses, action.payload])] };
     }
     case "removeDso": {
-      const addressToRemove = action.payload;
-      const dsosRemoved = prevDsos.filter(({ address }) => address !== addressToRemove);
-      const sortedDsos = dsosRemoved.sort(dsoComparator);
-
-      return { ...dsoState, dsos: sortedDsos };
+      const addressesRemoved = dsoState.dsoAddresses.filter((address) => address !== action.payload);
+      return { ...dsoState, dsoAddresses: addressesRemoved };
+    }
+    case "setDsoAddresses": {
+      return { ...dsoState, dsoAddresses: action.payload };
+    }
+    case "setDsoIds": {
+      return { ...dsoState, dsos: action.payload };
     }
     case "setAddDsoModal": {
       return { ...dsoState, addDsoModalState: action.payload };
+    }
+    case "setLeaveDsoModal": {
+      return { ...dsoState, leaveDsoModalState: action.payload };
     }
     default: {
       throw new Error("Unhandled action type");
@@ -82,10 +96,10 @@ function dsoReducer(dsoState: DsoState, action: DsoAction): DsoState {
 
 export function getDsoName(dsos: readonly DsoId[], dsoAddress: string): string {
   const dso = dsos.find(({ address }) => address === dsoAddress);
-  return dso?.name ?? "DSO";
+  return dso?.name ?? "Trusted Circle";
 }
-export function addDso(dispatch: DsoDispatch, dso: DsoId): void {
-  dispatch({ type: "addDso", payload: dso });
+export function addDso(dispatch: DsoDispatch, dsoAddress: string): void {
+  dispatch({ type: "addDso", payload: dsoAddress });
 }
 export function removeDso(dispatch: DsoDispatch, dsoAddress: string): void {
   dispatch({ type: "removeDso", payload: dsoAddress });
@@ -95,6 +109,12 @@ export function openAddDsoModal(dispatch: DsoDispatch): void {
 }
 export function closeAddDsoModal(dispatch: DsoDispatch): void {
   dispatch({ type: "setAddDsoModal", payload: "closed" });
+}
+export function openLeaveDsoModal(dispatch: DsoDispatch): void {
+  dispatch({ type: "setLeaveDsoModal", payload: "open" });
+}
+export function closeLeaveDsoModal(dispatch: DsoDispatch): void {
+  dispatch({ type: "setLeaveDsoModal", payload: "closed" });
 }
 
 export const useDso = (): NonNullable<DsoContextType> => {
@@ -109,27 +129,55 @@ export const useDso = (): NonNullable<DsoContextType> => {
 
 export default function DsoProvider({ children }: HTMLAttributes<HTMLOrSVGElement>): JSX.Element {
   const {
-    sdkState: { address },
+    sdkState: { address, signingClient },
   } = useSdk();
 
-  const [storedDsos, setStoredDsos] = useLocalStorage<DsosByUserMap>(
-    "trusted-circles",
+  const [storedAddresses, setStoredAddresses] = useLocalStorage<DsoAddressesByUserMap>(
+    "trusted-circles-addresses",
     new Map(),
     (map) => JSON.stringify(Array.from(map.entries())),
     (map) => new Map(JSON.parse(map)),
   );
-  const dsos = storedDsos.get(address) ?? [];
-  const [dsoState, dsoDispatch] = useReducer(dsoReducer, { dsos, addDsoModalState: "closed" });
+
+  const [dsoState, dsoDispatch] = useReducer(dsoReducer, {
+    dsoAddresses: storedAddresses.get(address) ?? [],
+    dsos: [],
+    addDsoModalState: "closed",
+    leaveDsoModalState: "closed",
+  });
 
   useEffect(() => {
-    setStoredDsos((prevStoredDsos) => prevStoredDsos.set(address, dsoState.dsos));
-  }, [address, dsoState.dsos, setStoredDsos]);
+    setStoredAddresses((prevAddresses) => prevAddresses.set(address, dsoState.dsoAddresses));
+  }, [address, dsoState.dsoAddresses, setStoredAddresses]);
+
+  useEffect(() => {
+    const dsoPromises = dsoState.dsoAddresses.map((address) =>
+      signingClient.queryContractSmart(address, { dso: {} }).then(({ name }) => ({ address, name })),
+    );
+
+    Promise.allSettled(dsoPromises).then((results) => {
+      const rejectedResults = results.filter((result) => result.status === "rejected");
+      const fulfilledResults = results.filter(
+        (result): result is PromiseFulfilledResult<DsoId> => result.status === "fulfilled",
+      );
+
+      if (rejectedResults.length) {
+        const dsoAddresses = fulfilledResults.map((result) => result.value.address);
+        dsoDispatch({ type: "setDsoAddresses", payload: dsoAddresses });
+      } else {
+        const dsos = fulfilledResults.map((result) => result.value);
+        dsoDispatch({ type: "setDsoIds", payload: dsos.sort(dsoComparator) });
+      }
+    });
+  }, [dsoState.dsoAddresses, signingClient]);
 
   return (
     <DsoContext.Provider value={{ dsoState, dsoDispatch }}>
       <>
         {children}
+
         <AddDsoModal />
+        <LeaveDsoModal />
       </>
     </DsoContext.Provider>
   );
