e5d21f65e63570c61ab026800f306b326ae549bf
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-07-05 12:36:42 +0200

    Adapt Dso switch to new router

--- src/App/routes/Dso/index.tsx
+++ src/App/routes/Dso/index.tsx
@@ -1,18 +1,16 @@
 import { paths } from "App/paths";
 import * as React from "react";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import DsoHome from "./routes/DsoHome";
 import EmptyDsos from "./routes/EmptyDsos";
 
 export default function Dso(): JSX.Element {
-  const { path: basePath } = useRouteMatch();
-
   return (
     <Switch>
-      <Route exact path={basePath}>
+      <Route exact path={paths.dso.prefix}>
         <EmptyDsos />
       </Route>
-      <Route path={`${basePath}${paths.dso.params.dsoAddress}`}>
+      <Route path={`${paths.dso.prefix}${paths.dso.params.dsoAddress}`}>
         <DsoHome />
       </Route>
     </Switch>
