46a095821b10f8e5591c5f8ee63aa94752a4b139
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-07-05 12:37:24 +0200

    Add StyledStack to EmptyDso

---
+++ src/App/routes/Dso/routes/EmptyDsos/style.ts
@@ -0,0 +1,8 @@
+import { Stack } from "App/components/layoutPrimitives";
+import styled from "styled-components";
+
+export const StyledStack = styled(Stack)`
+  .ant-btn {
+    align-self: center;
+  }
+`;
