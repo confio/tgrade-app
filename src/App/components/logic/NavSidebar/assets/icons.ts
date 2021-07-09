66f3a38cb8d928916d776640ddf640602344c974
Hannu Alexander <hannubit@gmail.com>
2021-07-09 15:48:42 +0200

    updated asset imports

--- src/App/components/logic/NavSidebar/assets/icons.ts
+++ src/App/components/logic/NavSidebar/assets/icons.ts
@@ -1,12 +1,12 @@
-import { ReactComponent as TrustedCircle } from "./assets/tc-icon.svg";
-import { ReactComponent as Token } from "./assets/token-icon.svg";
-import { ReactComponent as Wiki } from "./assets/wiki-icon.svg";
-import { ReactComponent as Feedback } from "./assets/feedback-icon.svg";
-import { ReactComponent as TgradeLogo } from "./assets/tgrade-logo.svg";
-import { ReactComponent as ConnectWallet } from "./assets/connect-wallet-icon.svg";
-import { ReactComponent as Trade } from "./assets/trade-icon.svg";
-import { ReactComponent as Join } from "./assets/join-icon.svg";
-import { ReactComponent as Learn } from "./assets/learn-icon.svg";
-import { ReactComponent as Visit } from "./assets/visit-icon.svg";
+import { ReactComponent as TrustedCircle } from "./tc-icon.svg";
+import { ReactComponent as Token } from "./token-icon.svg";
+import { ReactComponent as Wiki } from "./wiki-icon.svg";
+import { ReactComponent as Feedback } from "./feedback-icon.svg";
+import { ReactComponent as TgradeLogo } from "./tgrade-logo.svg";
+import { ReactComponent as ConnectWallet } from "/connect-wallet-icon.svg";
+import { ReactComponent as Trade } from "./trade-icon.svg";
+import { ReactComponent as Join } from "./join-icon.svg";
+import { ReactComponent as Learn } from "./learn-icon.svg";
+import { ReactComponent as Visit } from "./visit-icon.svg";
 
 export { TrustedCircle, Token, Wiki, Feedback, TgradeLogo, ConnectWallet, Trade, Join, Learn, Visit };
