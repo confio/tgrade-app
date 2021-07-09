34da9d40f2b006397a077a972d990556ca55f1c4
Hannu Alexander <hannubit@gmail.com>
2021-07-09 15:22:10 +0200

    renamed assets

--- src/service/layout.tsx
+++ src/service/layout.tsx
@@ -1,15 +1,11 @@
-import { CorporateBannerLayout } from "App/components/layout";
 import { NavSidebar } from "App/components/logic/NavSidebar";
-import { BackButton, Menu, RedirectLocation } from "App/components/logic";
+import { BackButton, RedirectLocation } from "App/components/logic";
 import { paths } from "App/paths";
 import * as React from "react";
 import { ComponentProps, createContext, HTMLAttributes, useContext, useEffect, useReducer } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
 import { hitFaucetIfNeeded, isSdkInitialized, useSdkInit } from "service";
-import { useWindowSize } from "utils/ui";
-
-type MenuState = "open" | "closed" | "hidden";
 
 type ViewTitles = {
   readonly viewTitle?: string;
@@ -17,14 +13,6 @@ type ViewTitles = {
 };
 
 type LayoutAction =
-  | {
-      readonly type: "setMenu";
-      readonly payload: MenuState;
-    }
-  | {
-      readonly type: "setCorporateBanner";
-      readonly payload: boolean;
-    }
   | {
       readonly type: "setBackButtonProps";
       readonly payload?: ComponentProps<typeof BackButton>;
@@ -40,8 +28,6 @@ type LayoutAction =
 
 type LayoutDispatch = (action: LayoutAction) => void;
 type LayoutState = {
-  readonly menuState: MenuState;
-  readonly showCorporateBanner: boolean;
   readonly backButtonProps?: ComponentProps<typeof BackButton>;
   readonly viewTitles?: ViewTitles;
   readonly isLoading: boolean;
@@ -59,12 +45,6 @@ const LayoutContext = createContext<LayoutContextType>(undefined);
 
 function layoutReducer(state: LayoutState, action: LayoutAction): LayoutState {
   switch (action.type) {
-    case "setMenu": {
-      return { ...state, menuState: action.payload };
-    }
-    case "setCorporateBanner": {
-      return { ...state, showCorporateBanner: action.payload };
-    }
     case "setBackButtonProps": {
       return { ...state, backButtonProps: action.payload };
     }
@@ -82,15 +62,6 @@ function layoutReducer(state: LayoutState, action: LayoutAction): LayoutState {
       throw new Error("Unhandled action type");
     }
   }
-}
-
-export const hideMenu = (dispatch: LayoutDispatch): void => dispatch({ type: "setMenu", payload: "hidden" });
-export const openMenu = (dispatch: LayoutDispatch): void => dispatch({ type: "setMenu", payload: "open" });
-export const closeMenu = (dispatch: LayoutDispatch): void => dispatch({ type: "setMenu", payload: "closed" });
-export const showMenu = closeMenu;
-
-export function setCorporateBanner(dispatch: LayoutDispatch, showCorporateBanner: boolean): void {
-  dispatch({ type: "setCorporateBanner", payload: showCorporateBanner });
 }
 
 export function setBackButtonProps(
@@ -113,24 +84,9 @@ type InitialLayoutState = {
 };
 
 export function setInitialLayoutState(dispatch: LayoutDispatch, state?: InitialLayoutState): void {
-  const { backButtonProps, viewTitles, menuState, showCorporateBanner } = state ?? {};
+  const { backButtonProps, viewTitles } = state ?? {};
   setBackButtonProps(dispatch, backButtonProps);
-  setCorporateBanner(dispatch, !!showCorporateBanner);
   setTitles(dispatch, viewTitles);
-
-  switch (menuState) {
-    case "open": {
-      openMenu(dispatch);
-      break;
-    }
-    case "hidden": {
-      hideMenu(dispatch);
-      break;
-    }
-    default: {
-      closeMenu(dispatch);
-    }
-  }
 }
 
 export const useLayout = (): NonNullable<LayoutContextType> => {
@@ -149,8 +105,6 @@ export default function LayoutProvider({ children }: HTMLAttributes<HTMLOrSVGEle
   const state = history.location.state as RedirectLocation;
   const { sdkState } = useSdkInit();
   const [layoutState, layoutDispatch] = useReducer(layoutReducer, {
-    menuState: "open",
-    showCorporateBanner: false,
     isLoading: false,
   });
 
@@ -169,9 +123,6 @@ export default function LayoutProvider({ children }: HTMLAttributes<HTMLOrSVGEle
       setLoading(layoutDispatch, false);
     })();
   }, [history, layoutState.loadingMsg, sdkState, state, t]);
-
-  const { width } = useWindowSize();
-  const showMenu = layoutState.menuState !== "hidden" && !layoutState.isLoading;
 
   return (
     <LayoutContext.Provider value={{ layoutState, layoutDispatch }}>
