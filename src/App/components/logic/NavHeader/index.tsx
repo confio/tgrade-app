f8efed6ea6f832053d76a4b0f7a9cbdc025552ff
Hannu Alexander <hannubit@gmail.com>
2021-07-09 15:47:38 +0200

    removed burgerbutton import

--- src/App/components/logic/NavHeader/index.tsx
+++ src/App/components/logic/NavHeader/index.tsx
@@ -1,5 +1,4 @@
 import BackButton from "../BackButton";
-import BurgerButton from "../BurgerButton";
 import * as React from "react";
 import { ComponentProps, HTMLAttributes } from "react";
 import { StyledNav } from "./style";
