ba27545a3f8f8c56b9ce06aba4f705a3ef47b263
Hannu Alexander <hannubit@gmail.com>
2021-07-09 15:47:05 +0200

    moved icons to central export

--- src/App/components/logic/NavSidebar/index.tsx
+++ src/App/components/logic/NavSidebar/index.tsx
@@ -1,58 +1,48 @@
 import { Link } from "react-router-dom";
-import { paths } from "App/paths";
-import { HTMLAttributes } from "react";
+import React from "react";
 import { Navbar, Cell, LinkWrapper, StyledText } from "./style";
-import { ReactComponent as TcIcon } from "./assets/tc-icon.svg";
-import { ReactComponent as TokenIcon } from "./assets/token-icon.svg";
-import { ReactComponent as WikiIcon } from "./assets/wiki-icon.svg";
-import { ReactComponent as FeedbackIcon } from "./assets/feedback-icon.svg";
-import { ReactComponent as TgradeLogo } from "./assets/tgrade-logo.svg";
-import { ReactComponent as ConnectWalletIcon } from "./assets/connect-wallet-icon.svg";
-import { ReactComponent as TradeIcon } from "./assets/trade-icon.svg";
-import { ReactComponent as JoinIcon } from "./assets/join-icon.svg";
-import { ReactComponent as LearnIcon } from "./assets/learn-icon.svg";
-import { ReactComponent as VisitIcon } from "./assets/visit-icon.svg";
+import * as Icon from "./assets/icons";
 
-export function NavSidebar({ children }: HTMLAttributes<HTMLOrSVGElement>): JSX.Element {
+export const NavSidebar: React.FC = () => {
   return (
     <Navbar>
       <LinkWrapper>
-        <TgradeLogo style={{ margin: "1.5rem" }} />
+        <Icon.TgradeLogo style={{ margin: "1.5rem" }} />
         <Link to="/">
           <Cell>
-            <TcIcon />
+            <Icon.TrustedCircle />
             <StyledText>Trusted Circle</StyledText>
-            <JoinIcon />
+            <Icon.Join />
           </Cell>
         </Link>
         <Link to="/">
           <Cell>
-            <TokenIcon />
+            <Icon.Token />
             <StyledText>T-Market</StyledText>
-            <TradeIcon />
+            <Icon.Trade />
           </Cell>
         </Link>
         <Link to="/">
           <Cell>
-            <WikiIcon />
+            <Icon.Wiki />
             <StyledText>Wiki</StyledText>
-            <LearnIcon />
+            <Icon.Wiki />
           </Cell>
         </Link>
         <Link to="/">
           <Cell>
-            <FeedbackIcon />
+            <Icon.Feedback />
             <StyledText>Feedback</StyledText>
-            <VisitIcon />
+            <Icon.Visit />
           </Cell>
         </Link>
       </LinkWrapper>
       <Link to="/">
         <Cell>
-          <ConnectWalletIcon />
+          <Icon.ConnectWallet />
           <StyledText>Connect Wallet</StyledText>
         </Cell>
       </Link>
     </Navbar>
   );
-}
+};
