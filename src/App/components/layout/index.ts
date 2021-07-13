5b02db45830057399c8095dc0d6eb9b5fe0e5eed
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-07-13 13:35:40 +0200

    Remove unneeded OldPageLayout

--- src/App/components/layout/index.ts
+++ src/App/components/layout/index.ts
@@ -1,3 +1,2 @@
 export { default as CorporateBannerLayout } from "./CorporateBannerLayout";
-export { default as OldPageLayout } from "./OldPageLayout";
 export { default as PageLayout } from "./PageLayout";
