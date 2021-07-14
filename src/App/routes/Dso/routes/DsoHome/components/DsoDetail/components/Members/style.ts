4b37064f77c0cceb888f30ab103ffd2f6804f544
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-07-14 12:16:17 +0200

    Add styles for members widget

--- src/App/routes/Dso/routes/DsoHome/components/DsoDetail/components/Members/style.ts
+++ src/App/routes/Dso/routes/DsoHome/components/DsoDetail/components/Members/style.ts
@@ -12,3 +12,36 @@ export const MembersStack = styled(Stack)`
     font-size: var(--s1);
   }
 `;
+
+export const MemberCounts = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-evenly;
+  gap: var(--s0);
+`;
+
+export const MemberCount = styled.div`
+  padding: var(--s1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex-basis: 14rem;
+  min-height: 10rem;
+  border: 1px solid var(--color-border);
+  border-radius: var(--border-radius);
+
+  &:first-child span.ant-typography:first-child {
+    color: var(--color-primary);
+  }
+
+  span.ant-typography:first-child {
+    font-size: var(--s5);
+    font-weight: 500;
+  }
+
+  span.ant-typography:nth-child(2) {
+    color: var(--color-text-1ary);
+  }
+`;
