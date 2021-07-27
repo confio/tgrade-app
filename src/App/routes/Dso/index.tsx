34d267ae18713d39c64facf2e2a57f6c0f9802f9
hannuprod <hannubit@gmail.com>
2021-07-27 08:56:32 +0200

    (wip) converted routes to pages

--- src/App/routes/Dso/index.tsx
+++ src/App/routes/Dso/index.tsx
@@ -2,7 +2,7 @@ import { paths } from "App/paths";
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
 import DsoHome from "./routes/DsoHome";
-import EmptyDsos from "./routes/EmptyDsos";
+import EmptyDsos from "../../pages/EmptyDsos";
 
 export default function Dso(): JSX.Element {
   return (
