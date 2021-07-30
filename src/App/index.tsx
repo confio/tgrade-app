37aae3861ebeec96c2fdab61ce29956fb03bafbf
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-07-31 00:12:24 +0200

    Add store index and Provider

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -6,36 +6,40 @@ import {
   QueryClient as ReactQueryClient,
   QueryClientProvider as ReactQueryClientProvider,
 } from "react-query";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import { DsoProvider, ErrorProvider, LayoutProvider, SdkProvider, ThemeProvider } from "service";
+import { store } from "store";
 import { paths } from "./paths";
 import Dso from "./routes/Dso";
 
 export default function App(): JSX.Element {
   return (
-    <I18nextProvider i18n={i18n}>
-      <ErrorProvider>
-        <ReactQueryClientProvider client={new ReactQueryClient()}>
-          <SdkProvider config={config}>
-            <ThemeProvider>
-              <Router basename={process.env.PUBLIC_URL}>
-                <LayoutProvider>
-                  <Switch>
-                    <Route exact path="/">
-                      <Redirect to={paths.dso.prefix} />
-                    </Route>
-                    <Route path={`${paths.dso.prefix}${paths.dso.params.dsoAddressOptional}`}>
-                      <DsoProvider>
-                        <Dso />
-                      </DsoProvider>
-                    </Route>
-                  </Switch>
-                </LayoutProvider>
-              </Router>
-            </ThemeProvider>
-          </SdkProvider>
-        </ReactQueryClientProvider>
-      </ErrorProvider>
-    </I18nextProvider>
+    <Provider store={store}>
+      <I18nextProvider i18n={i18n}>
+        <ErrorProvider>
+          <ReactQueryClientProvider client={new ReactQueryClient()}>
+            <SdkProvider config={config}>
+              <ThemeProvider>
+                <Router basename={process.env.PUBLIC_URL}>
+                  <LayoutProvider>
+                    <Switch>
+                      <Route exact path="/">
+                        <Redirect to={paths.dso.prefix} />
+                      </Route>
+                      <Route path={`${paths.dso.prefix}${paths.dso.params.dsoAddressOptional}`}>
+                        <DsoProvider>
+                          <Dso />
+                        </DsoProvider>
+                      </Route>
+                    </Switch>
+                  </LayoutProvider>
+                </Router>
+              </ThemeProvider>
+            </SdkProvider>
+          </ReactQueryClientProvider>
+        </ErrorProvider>
+      </I18nextProvider>
+    </Provider>
   );
 }
---
+++ src/store/index.ts
@@ -0,0 +1,9 @@
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { rootReducer } from "./reducers";
+import { rootSaga } from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+export const store = createStore(rootReducer, applyMiddleware(sagaMiddleware, logger));
+sagaMiddleware.run(rootSaga);
