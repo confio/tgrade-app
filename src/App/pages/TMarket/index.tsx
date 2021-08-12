946ed0e04d9f131c3bc6fc0fcde951986a0bd03b
hannuprod <hannubit@gmail.com>
2021-08-12 12:12:39 +0300

    Added Chart template

--- src/App/pages/TMarket/index.tsx
+++ src/App/pages/TMarket/index.tsx
@@ -1,3 +1,12 @@
+import Chart from "App/components/Chart";
+
 export default function TMarketHome(): JSX.Element | null {
-  return <div>Hello</div>;
+  return (
+    <div style={{ width: "90%" }}>
+      <div style={{ width: "50%" }}>
+        <h2>Welcome to T-Market</h2>
+        <Chart />
+      </div>
+    </div>
+  );
 }
