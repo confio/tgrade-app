178956f2b3d3f0b067d764b4d8604ce1fe98d423
hannuprod <hannubit@gmail.com>
2021-08-12 15:25:21 +0300

    chart mock data

--- src/App/components/Chart/index.tsx
+++ src/App/components/Chart/index.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { Line } from "@ant-design/charts";
 
-interface ChartProps {
-  config: {
-    data: [];
-    xField: string;
-    yField: string;
-    color: [];
-  };
-}
-
-const Chart: React.FC<ChartProps> = ({ config }) => {
-  return <Line {...config} />;
+const data = [
+  { month: "Jan", value: 3 },
+  { month: "Feb", value: 3 },
+  { month: "March", value: 3 },
+  { month: "April", value: 4 },
+  { month: "May", value: 3.5 },
+  { month: "June", value: 5 },
+  { month: "July", value: 4.9 },
+  { month: "August", value: 6 },
+  { month: "September", value: 7 },
+  { month: "October", value: 9 },
+  { month: "November", value: 13 },
+];
+const config = {
+  data,
+  xField: "month",
+  yField: "value",
+  color: ["#1979C9", "#D62A0D", "#FAA219"],
 };
+
+function Chart(): JSX.Element {
+  return <Line {...config} />;
+}
 export default Chart;
--- src/App/pages/TMarket/index.tsx
+++ src/App/pages/TMarket/index.tsx
@@ -1,23 +1,4 @@
 import Chart from "App/components/Chart";
-const data = [
-  { month: "Jan", value: 3 },
-  { month: "Feb", value: 3 },
-  { month: "March", value: 3 },
-  { month: "April", value: 4 },
-  { month: "May", value: 3.5 },
-  { month: "June", value: 5 },
-  { month: "July", value: 4.9 },
-  { month: "August", value: 6 },
-  { month: "September", value: 7 },
-  { month: "October", value: 9 },
-  { month: "November", value: 13 },
-];
-const config = {
-  data,
-  xField: "month",
-  yField: "value",
-  color: ["#1979C9", "#D62A0D", "#FAA219"],
-};
 
 export default function TMarketHome(): JSX.Element | null {
   return (
@@ -25,7 +6,7 @@ export default function TMarketHome(): JSX.Element | null {
       <div style={{ width: "50%" }}>
         <h2>Welcome to T-Market</h2>
         <h3>What is T-market?</h3>
-        <Chart props={config} />
+        <Chart />
       </div>
     </div>
   );
