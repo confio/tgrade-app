5da71b0a333dfabd08f1b92506bd1e6263777bb9
hannuprod <hannubit@gmail.com>
2021-08-12 12:16:27 +0300

    (WIP)fleshing out wrappers

--- src/App/pages/TMarket/index.tsx
+++ src/App/pages/TMarket/index.tsx
@@ -5,6 +5,7 @@ export default function TMarketHome(): JSX.Element | null {
     <div style={{ width: "90%" }}>
       <div style={{ width: "50%" }}>
         <h2>Welcome to T-Market</h2>
+        <h3>What is T-market?</h3>
         <Chart />
       </div>
     </div>
