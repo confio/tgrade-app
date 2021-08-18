78aab83026257af8234cbc45cde01c2e918a0a45
hannuprod <hannubit@gmail.com>
2021-08-18 09:44:40 +0300

    added tabs to liquidity container

--- src/App/containers/LiquidyContainer/index.tsx
+++ src/App/containers/LiquidyContainer/index.tsx
@@ -1,17 +1,30 @@
 import Table from "App/components/Table";
 import { columns, data } from "./__mocks__/MockData";
-import { LiquidityWrapper } from "./style";
+import { LiquidityWrapper, StyledTabs } from "./style";
 import { ReactComponent as TgradeLogo } from "App/assets/icons/tgradeLogo.svg";
 
 export default function LiquidyContainer(): JSX.Element | null {
   function onChange(pagination: any, filters: any, sorter: any, extra: any) {
     console.log("params", pagination, filters, sorter, extra);
   }
+
+  const { TabPane } = StyledTabs;
+
+  function callback(key: string) {
+    console.log(key);
+  }
   return (
     <LiquidityWrapper>
-      <div style={{ width: "100%", borderRadius: "16px" }}>
-        <Table columns={columns} dataSource={data} onChange={onChange} />
-      </div>
+      <StyledTabs defaultActiveKey="1" onChange={callback}>
+        <TabPane tab="Trading" key="1">
+          <div style={{ width: "100%", borderRadius: "16px" }}>
+            <Table columns={columns} dataSource={data} onChange={onChange} />
+          </div>
+        </TabPane>
+        <TabPane tab="Liquidity" key="2">
+          <span>Sea of Liquidity</span>
+        </TabPane>
+      </StyledTabs>
     </LiquidityWrapper>
   );
 }
