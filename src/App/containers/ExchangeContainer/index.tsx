e331c953d399046f2d9cc9f7f35618f1639f6306
hannuprod <hannubit@gmail.com>
2021-08-23 16:10:49 +0300

    added route to exchange button

--- src/App/containers/ExchangeContainer/index.tsx
+++ src/App/containers/ExchangeContainer/index.tsx
@@ -1,11 +1,21 @@
 import Button from "App/components/Button";
+import { useHistory } from "react-router";
 import ExchangeList from "App/components/ExchangeList";
+import { paths } from "App/paths";
 
 export default function ExchangeContainer(): JSX.Element | null {
+  const history = useHistory();
+
+  function handleClick() {
+    history.push(`${paths.tmarket.prefix}${paths.tmarket.exchange.prefix}`);
+  }
   return (
     <div>
       <ExchangeList />
-      <Button style={{ height: "42px", margin: "5px", alignItems: "center", float: "right" }}>
+      <Button
+        onClick={handleClick}
+        style={{ height: "42px", margin: "5px", alignItems: "center", float: "right" }}
+      >
         Exchange
       </Button>
     </div>
