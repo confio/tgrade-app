dc4bdd206224ae5deef046dd83d48cfaa9aaa679
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-09-06 17:48:41 +0200

    Add first part of split form

---
+++ src/App/containers/IssueTokenForm/components/TokenSpecs/style.ts
@@ -0,0 +1,29 @@
+import Stack from "App/components/Stack/style";
+import styled from "styled-components";
+
+export const Separator = styled.hr`
+  margin: var(--s1) -20px;
+  border: none;
+  border-top: 1px solid var(--color-input-border);
+`;
+
+export const FormStack = styled(Stack)`
+  .ant-form-item {
+    margin-bottom: 0;
+  }
+`;
+
+export const NameWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: var(--s0);
+
+  .ant-form-item {
+    flex-basis: 17rem;
+  }
+
+  .ant-form-item + .ant-form-item {
+    flex-basis: auto;
+    flex-grow: 1;
+  }
+`;
