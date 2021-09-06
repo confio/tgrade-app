451c0a44f3c6b46a0098cbe50aed129d8af2441a
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-09-06 17:47:46 +0200

    Add back button to ModalButtons

--- src/App/components/ModalButtons/style.ts
+++ src/App/components/ModalButtons/style.ts
@@ -2,5 +2,10 @@ import styled from "styled-components";
 
 export const StyledModalButtons = styled.div`
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  button:last-child {
+    margin-left: auto;
+  }
 `;
