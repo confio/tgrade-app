1b291c7be8d86c4e4ef8980e036e5ecf82507bea
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-09-06 17:48:25 +0200

    Adapt IssueTokenForm to split form

--- src/App/containers/IssueTokenForm/index.tsx
+++ src/App/containers/IssueTokenForm/index.tsx
@@ -1,72 +1,17 @@
 import { Decimal, Uint64 } from "@cosmjs/math";
-import Field from "App/components/Field";
 import { TxResult } from "App/components/ShowTxResult";
-import Stack from "App/components/Stack/style";
-import { Formik } from "formik";
-import { Form } from "formik-antd";
 import * as React from "react";
+import { useState } from "react";
 import { useError, useSdk } from "service";
-import { Contract20WS, MinterInterface } from "utils/cw20";
+import { Contract20WS, EmbeddedLogoType, LogoType, MinterInterface } from "utils/cw20";
 import { getErrorFromStackTrace } from "utils/errors";
-import { getFormItemName } from "utils/forms";
-import * as Yup from "yup";
-import FieldsTokenLogo from "../../components/FieldsTokenLogo";
-import FieldsTokenMint, { MintType } from "../../components/FieldsTokenMint";
-import FieldsTokenSupply from "../../components/FieldsTokenSupply";
-import ModalButtons from "../../components/ModalButtons";
-import ModalHeader from "../../components/ModalHeader";
-import { DsoWrapper, FormStack, NameWrapper, Separator } from "./style";
+import TokenMarketing, { FormMarketingFields } from "./components/TokenMarketing";
+import TokenSpecs, { FormTokenSpecsFields } from "./components/TokenSpecs";
 
-export interface FormIssueTokenFields {
-  readonly tokenSymbol: string;
-  readonly tokenName: string;
-  readonly mint: string;
-  readonly mintCap?: string;
-  readonly initialSupply: string;
-  readonly decimals: string;
+enum IssueTokenSteps {
+  Specs = "Specs",
+  Marketing = "Marketing",
 }
-
-const tokenSymbolLabel = "Token symbol";
-const tokenNameLabel = "Token name";
-const logoUrlLabel = "URL of the logo SVG image";
-const logoFileLabel = "Upload Logo (SVG)";
-const dsoAddressLabel = "Trusted Circle to associate with";
-const mintLabel = "Mint";
-const mintCapLabel = "Mint cap";
-const initialSupplyLabel = "Initial supply";
-const decimalsLabel = "Set decimal places";
-
-const validationSchema = Yup.object().shape({
-  [getFormItemName(tokenSymbolLabel)]: Yup.string()
-    .typeError("Token symbol must be alphanumeric")
-    .required("Token symbol is required")
-    .min(3, "Token symbol must have between 3 and 6 characters")
-    .max(6, "Token symbol must have between 3 and 6 characters"),
-  [getFormItemName(tokenNameLabel)]: Yup.string()
-    .typeError("Token name must be alphanumeric")
-    .required("Token name is required")
-    .min(3, "Token name must have between 3 and 30 characters")
-    .max(30, "Token name must have between 3 and 30 characters"),
-  [getFormItemName(mintCapLabel)]: Yup.number()
-    .typeError("Mint cap must be a number")
-    .positive("Mint cap must be positive")
-    .when(getFormItemName(initialSupplyLabel), (tokenSupply: number, schema: any) => {
-      return schema.test({
-        test: (tokenMintCap?: number) => !tokenMintCap || tokenSupply <= tokenMintCap,
-        message: "Mint cap must be equal or greater than initial supply",
-      });
-    }),
-  [getFormItemName(initialSupplyLabel)]: Yup.number()
-    .typeError("Initial supply must be a number")
-    .required("Initial supply is required")
-    .positive("Initial supply must be positive"),
-  [getFormItemName(decimalsLabel)]: Yup.number()
-    .typeError("Decimals must be a number")
-    .required("Decimals are required")
-    .integer("Decimals must be an integer")
-    .min(0, "Decimals must be between 0 and 18")
-    .max(18, "Decimals must be between 0 and 18"),
-});
 
 interface IssueTokenFormProps {
   readonly setTxResult: React.Dispatch<React.SetStateAction<TxResult | undefined>>;
@@ -79,36 +24,73 @@ export default function IssueTokenForm({ setTxResult, closeModal }: IssueTokenFo
     sdkState: { config, address, signingClient },
   } = useSdk();
 
-  async function handleSubmit(values: FormIssueTokenFields) {
-    if (!signingClient || !address || !config.codeIds?.cw20Tokens?.length) return;
-    console.log(values);
+  const [issueTokenStep, setIssueTokenStep] = useState(IssueTokenSteps.Specs);
+
+  const [tokenSymbol, setTokenSymbol] = useState("");
+  const [tokenName, setTokenName] = useState("");
+  const [logoUrl, setLogoUrl] = useState<string>();
+  const [logoFile, setLogoFile] = useState<EmbeddedLogoType>();
+  const [initialSupply, setInitialSupply] = useState("");
+  const [decimals, setDecimals] = useState("");
+  const [mint, setMint] = useState("");
+  const [mintCap, setMintCap] = useState<string>();
+
+  async function submitTokenSpecs(values: FormTokenSpecsFields) {
+    setTokenSymbol(values.tokenSymbol);
+    setTokenName(values.tokenName);
+    setInitialSupply(values.initialSupply);
+    setDecimals(values.decimals);
+    setMint(values.mint);
+    setMintCap(values.mintCap);
+
+    setIssueTokenStep(IssueTokenSteps.Marketing);
+  }
+
+  async function submitTokenMarketing(values: FormMarketingFields) {
+    if (
+      !signingClient ||
+      !address ||
+      !config.codeIds?.cw20Tokens?.length ||
+      !config.codeIds?.tgradeCw20?.length
+    )
+      return;
 
     try {
-      const decimals = parseInt(values.decimals, 10);
-      const amount = Decimal.fromUserInput(values.initialSupply, decimals)
-        .multiply(Uint64.fromNumber(10 ** decimals))
+      const decimalsNumber = parseInt(decimals, 10);
+      const amount = Decimal.fromUserInput(initialSupply, decimalsNumber)
+        .multiply(Uint64.fromNumber(10 ** decimalsNumber))
         .toString();
-      const cap = values.mintCap
-        ? Decimal.fromUserInput(values.mintCap, decimals)
-            .multiply(Uint64.fromNumber(10 ** decimals))
+      const cap = mintCap
+        ? Decimal.fromUserInput(mintCap, decimalsNumber)
+            .multiply(Uint64.fromNumber(10 ** decimalsNumber))
             .toString()
         : undefined;
-      const canMint = values.mint === "fixed" || values.mint === "unlimited";
-      const mint: MinterInterface | undefined = canMint ? { minter: address, cap } : undefined;
+      const canMint = mint === "fixed" || mint === "unlimited";
+      const minter: MinterInterface | undefined = canMint ? { minter: address, cap } : undefined;
 
-      const tokenName = values.tokenName;
-      const tokenSymbol = values.tokenSymbol;
+      const codeId = values.dsoAddress ? config.codeIds.tgradeCw20[0] : config.codeIds.cw20Tokens[0];
+      const logo: LogoType | undefined = logoUrl
+        ? { url: logoUrl }
+        : logoFile
+        ? { embedded: logoFile }
+        : undefined;
+      const marketing =
+        values.project || values.description || logo
+          ? { project: values.project, description: values.description, marketing: address, logo }
+          : undefined;
 
       const contractAddress = await Contract20WS.createContract(
         signingClient,
-        config.codeIds.cw20Tokens[0],
+        codeId,
         address,
         tokenName,
         tokenSymbol,
-        decimals,
+        decimalsNumber,
         [{ address, amount }],
-        mint,
+        minter,
         config.gasPrice,
+        marketing,
+        values.dsoAddress,
       );
 
       setTxResult({
@@ -122,63 +104,21 @@ export default function IssueTokenForm({ setTxResult, closeModal }: IssueTokenFo
   }
 
   return (
-    <Formik
-      initialValues={{
-        [getFormItemName(tokenSymbolLabel)]: "",
-        [getFormItemName(tokenNameLabel)]: "",
-        [getFormItemName(mintLabel)]: MintType.None,
-        [getFormItemName(mintCapLabel)]: "",
-        [getFormItemName(initialSupplyLabel)]: "",
-        [getFormItemName(decimalsLabel)]: "",
-      }}
-      enableReinitialize
-      validationSchema={validationSchema}
-      onSubmit={(values) =>
-        handleSubmit({
-          tokenSymbol: values[getFormItemName(tokenSymbolLabel)].toString(),
-          tokenName: values[getFormItemName(tokenNameLabel)].toString(),
-          mint: values[getFormItemName(mintLabel)].toString(),
-          mintCap: values[getFormItemName(mintCapLabel)].toString(),
-          initialSupply: values[getFormItemName(initialSupplyLabel)].toString(),
-          decimals: values[getFormItemName(decimalsLabel)].toString(),
-        })
-      }
-    >
-      {({ isValid, submitForm, isSubmitting, values, errors }) => {
-        return (
-          <Stack gap="s2">
-            <ModalHeader title="Create digital asset" isSubmitting={isSubmitting} closeModal={closeModal} />
-            <Separator />
-            <Form>
-              <FormStack gap="s1">
-                <NameWrapper>
-                  <Field label={tokenSymbolLabel} placeholder="Enter token symbol" />
-                  <Field label={tokenNameLabel} placeholder="Enter token name" />
-                </NameWrapper>
-                <FieldsTokenLogo logoUrlLabel={logoUrlLabel} logoFileLabel={logoFileLabel} />
-                <DsoWrapper>
-                  <Field label={dsoAddressLabel} placeholder="Enter address" />
-                </DsoWrapper>
-                <Separator />
-                <FieldsTokenSupply
-                  formikValues={values}
-                  formikErrors={errors}
-                  initialSupplyLabel={initialSupplyLabel}
-                  decimalsLabel={decimalsLabel}
-                  tokenSymbolLabel={tokenSymbolLabel}
-                />
-                <FieldsTokenMint mintLabel={mintLabel} mintCapLabel={mintCapLabel} />
-                <Separator />
-                <ModalButtons
-                  buttonPrimaryTitle="Create asset"
-                  buttonPrimaryDisabled={!isValid}
-                  buttonPrimaryOnClick={() => submitForm()}
-                />
-              </FormStack>
-            </Form>
-          </Stack>
-        );
-      }}
-    </Formik>
+    <>
+      {issueTokenStep === IssueTokenSteps.Specs ? (
+        <TokenSpecs
+          closeModal={closeModal}
+          setLogoUrl={setLogoUrl}
+          setLogoFile={setLogoFile}
+          handleSubmit={submitTokenSpecs}
+        />
+      ) : issueTokenStep === IssueTokenSteps.Marketing ? (
+        <TokenMarketing
+          closeModal={closeModal}
+          handleSubmit={submitTokenMarketing}
+          goBack={() => setIssueTokenStep(IssueTokenSteps.Specs)}
+        />
+      ) : null}
+    </>
   );
 }
