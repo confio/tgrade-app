1455f977c8d564c55ebe7b9d80793a0311b5533d
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-09-09 12:43:58 +0200

    Add support for factory instantiation over network

--- scripts/tgrade/instantiate_factory.mjs
+++ scripts/tgrade/instantiate_factory.mjs
@@ -8,6 +8,25 @@ import { FaucetClient } from "@cosmjs/faucet-client";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { calculateFee, GasPrice, makeCosmoshubPath } from "@cosmjs/stargate";
 
+/*
+Usage:
+  1. Configure the network in which to instantiate the factory by filling the networkConfig object accordingly.
+  2. Run "./instantiate_factory.mjs network" and copy the generated factory address.
+  3. Hardcode that address in src/config/network.ts.
+*/
+
+const networkConfig = {
+  endpoint: "https://rpc.testnet-2.tgrade.io",
+  faucet: "https://faucet.testnet-2.tgrade.io",
+  bech32prefix: "tgrade",
+  feeDenom: "utgd",
+  gasPrice: GasPrice.fromString("0.025utgd"),
+  gasLimitCreateFactory: 500_000,
+  cw20LiquidityCodeId: 6,
+  factoryCodeId: 8,
+  pairCodeId: 9,
+};
+
 const localConfig = {
   endpoint: "http://localhost:26657",
   faucet: "http://localhost:8000",
@@ -20,7 +39,7 @@ const localConfig = {
   pairCodeId: 9,
 };
 
-const config = localConfig;
+const config = process.argv[2] === "network" ? networkConfig : localConfig;
 
 async function main() {
   // build signing client
