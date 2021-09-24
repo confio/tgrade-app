683f651489e7e74b30f22e9e8429ae8a4606b8e2
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-09-24 13:07:05 +0200

    Remove text from T-Market (#101)

--- src/App/pages/TMarket/index.tsx
+++ src/App/pages/TMarket/index.tsx
@@ -2,25 +2,18 @@ import Stack from "App/components/Stack/style";
 import IssueTokenModal from "App/containers/IssueTokenModal";
 import { useState } from "react";
 import TMarket from "./routes";
-import { LightButton, LinkText, PageWrapper, Text, Title, TitleWrapper } from "./style";
+import { LightButton, PageWrapper, Title, TitleWrapper } from "./style";
 
 export default function TMarketHome(): JSX.Element | null {
   const [isModalOpen, setModalOpen] = useState(false);
 
   return (
     <PageWrapper>
-      <Stack>
+      <Stack gap="s4">
         <TitleWrapper>
           <Title>Welcome to T-Market</Title>
           <LightButton onClick={() => setModalOpen(true)}>Create Asset</LightButton>
         </TitleWrapper>
-        <Text>
-          T-Market is an Automated Market Maker (AMM) which is constantly running and will always give a price
-          for the listed pairs.
-        </Text>
-        <LinkText href="https://confio.github.io/tgrade-docs/docs/onboarding/T-Market/">
-          What is T-market?
-        </LinkText>
         <TMarket />
       </Stack>
       <IssueTokenModal isModalOpen={isModalOpen} closeModal={() => setModalOpen(false)} />
