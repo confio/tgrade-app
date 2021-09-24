57a687b44eec9e5bd22db2def0045f0b191fb61c
Hannu <41473010+HannuProd@users.noreply.github.com>
2021-09-24 15:33:19 +0200

    Landing page (#102)
    * added google fonts
    
    * added links
    
    * added footer
    
    * changed spacing
    
    * removed console.log

---
+++ src/App/pages/LandingPage/index.tsx
@@ -0,0 +1,100 @@
+import {
+  Header,
+  PageWrapper,
+  LogoWrapper,
+  Footer,
+  ContentWrapper,
+  Text,
+  TextSmall,
+  LinkButton,
+  ContactForm,
+  Paragraph,
+  SubscribeButton,
+} from "./style";
+import { ReactComponent as TgradeLogo } from "App/assets/icons/tgrade-logo.svg";
+import { ReactComponent as TwitterLogo } from "App/assets/icons/twitter-icon.svg";
+import { ReactComponent as LinkedinLogo } from "App/assets/icons/linkedin-icon.svg";
+import { Link } from "react-router-dom";
+
+export default function LandingPage(): JSX.Element | null {
+  return (
+    <PageWrapper>
+      <Header>
+        <TgradeLogo style={{ marginLeft: "100px" }} />
+        <LogoWrapper>
+          <a href="https://twitter.com/TgradeFinance">
+            <TwitterLogo />
+          </a>
+          <a href="https://www.linkedin.com/company/tgrade">
+            <LinkedinLogo />
+          </a>
+        </LogoWrapper>
+      </Header>
+      <ContentWrapper>
+        <span style={{ marginLeft: "2rem", width: "650px" }}>
+          <Text>
+            <b>Try Tgrade</b> and see for yourself - the power of Blockchain is closer than you think.
+          </Text>
+        </span>
+        <Link to="/dso">
+          <LinkButton>
+            <TextSmall>
+              Try the <b>Tgrade App</b>
+            </TextSmall>
+          </LinkButton>
+        </Link>
+        <a href="https://outlook.office365.com/owa/calendar/Tgrade@confio.gmbh/bookings/s/WgrBVH4R9Umi2xs9SHsWqw2">
+          <LinkButton>
+            <TextSmall>
+              Book a <b>Demo of Tgrade</b>
+            </TextSmall>
+          </LinkButton>
+        </a>
+        <Link to="/doc">
+          <LinkButton>
+            <TextSmall>
+              Read the <b>Documentation</b>
+            </TextSmall>
+          </LinkButton>
+        </Link>
+        <a href="https://tgrade.finance/">
+          <LinkButton>
+            <TextSmall>
+              Learn more about <b>Tgrade</b>
+            </TextSmall>
+          </LinkButton>
+        </a>
+      </ContentWrapper>
+      <Footer>
+        <div style={{ display: "flex", flexDirection: "column", marginLeft: "100px" }}>
+          <TgradeLogo style={{ width: "94px", marginBottom: "16px" }} />
+          <Paragraph>©2021 by Confio.gmbh</Paragraph>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginLeft: "100px",
+            marginRight: "100px",
+            marginTop: "46px",
+          }}
+        >
+          <a href="https://tgrade.finance/impressum/">
+            <Paragraph>Legal Information / Impressum</Paragraph>
+          </a>
+          <div style={{ width: "200px" }} />
+          <a href="https://tgrade.finance/privacy-policy">
+            <Paragraph>Privacy Policy</Paragraph>
+          </a>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Paragraph>NEWSLETTER</Paragraph>
+          <ContactForm>
+            <Paragraph style={{ marginLeft: "13px", color: "#8692A6" }}>Enter your email address</Paragraph>
+            <SubscribeButton>Subscribe</SubscribeButton>
+          </ContactForm>
+        </div>
+      </Footer>
+    </PageWrapper>
+  );
+}
---
+++ src/App/pages/LandingPage/style.ts
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+import Button from "App/components/Button";
+import LandingBG from "App/assets/images/landing-page-background.png";
+
+export const PageWrapper = styled.div`
+  background: url(${LandingBG});
+  background-size: cover;
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  height: 80px;
+`;
+
+export const LogoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 110px;
+  margin-right: 100px;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 132px;
+  position: fixed;
+  align-items: center;
+  justify-content: space-between;
+  bottom: 0;
+  background: #242730;
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 50%;
+  margin-top: 5%;
+`;
+
+export const Text = styled.h2`
+  font-family: Montserrat;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 50px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #fff;
+`;
+
+export const TextSmall = styled.h2`
+  font-family: Montserrat;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 50px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #fff;
+`;
+
+export const LinkButton = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 650px;
+  height: 74px;
+  border: 1px solid #ffffff;
+  box-sizing: border-box;
+  border-radius: 106px;
+  margin-top: 20px;
+  cursor: pointer;
+`;
+
+export const ContactForm = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 333px;
+  height: 42px;
+  border: 1px solid #8692a6;
+  box-sizing: border-box;
+  border-radius: 6px 36px 36px 6px;
+  margin-right: 100px;
+`;
+
+export const Paragraph = styled.p`
+  font-family: Montserrat;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 30px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #fff;
+`;
+
+export const SubscribeButton = styled(Button)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100px;
+  height: 36px;
+  border-radius: 36px;
+  margin-right: 1px;
+  font-family: Quicksand;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: 0em;
+  text-align: center;
+`;
