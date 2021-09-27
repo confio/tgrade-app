b8010f293892ffcb447ca5d9b61ecbbebeee87df
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-09-27 09:08:39 +0200

    Add Toast to TMarketHome

--- src/App/pages/TMarket/index.tsx
+++ src/App/pages/TMarket/index.tsx
@@ -1,19 +1,34 @@
 import Stack from "App/components/Stack/style";
 import IssueTokenModal from "App/containers/IssueTokenModal";
+import { paths } from "App/paths";
 import { useState } from "react";
+import { useLocation } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import TMarket from "./routes";
-import { LightButton, PageWrapper, Title, TitleWrapper } from "./style";
+import { LightButton, NotificationsContainer, PageWrapper, Title, TitleWrapper } from "./style";
 
 export default function TMarketHome(): JSX.Element | null {
+  const { pathname } = useLocation();
   const [isModalOpen, setModalOpen] = useState(false);
 
   return (
     <PageWrapper>
       <Stack gap="s4">
-        <TitleWrapper>
-          <Title>Welcome to T-Market</Title>
-          <LightButton onClick={() => setModalOpen(true)}>Create Asset</LightButton>
-        </TitleWrapper>
+        {!pathname.endsWith(paths.tmarket.exchange.result) ? (
+          <TitleWrapper>
+            <Title>Welcome to T-Market</Title>
+            <LightButton onClick={() => setModalOpen(true)}>Create Asset</LightButton>
+          </TitleWrapper>
+        ) : null}
+        <NotificationsContainer id="notifications_container">
+          <ToastContainer
+            newestOnTop={true}
+            position="bottom-right"
+            hideProgressBar={true}
+            autoClose={false}
+          />
+        </NotificationsContainer>
         <TMarket />
       </Stack>
       <IssueTokenModal isModalOpen={isModalOpen} closeModal={() => setModalOpen(false)} />
