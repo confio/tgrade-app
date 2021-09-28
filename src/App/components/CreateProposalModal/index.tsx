fd929712b47a40190bef8e237eae01bc902a395e
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-09-28 11:31:05 +0200

    Add lazy Proposal types

--- src/App/components/CreateProposalModal/index.tsx
+++ src/App/components/CreateProposalModal/index.tsx
@@ -4,19 +4,19 @@ import modalBg from "App/assets/images/modal-background.jpg";
 import { TxResult } from "App/components/ShowTxResult";
 import Stack from "App/components/Stack/style";
 import Steps from "App/components/Steps";
-import * as React from "react";
-import { useState } from "react";
+import { lazy, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getDsoName, useDso } from "service";
 import { DsoHomeParams } from "../../pages/DsoHome";
-import ProposalAddParticipants from "./components/ProposalAddParticipants";
-import ProposalAddVotingParticipants from "./components/ProposalAddVotingParticipants";
-import ProposalEditDso from "./components/ProposalEditDso";
-import ProposalRemoveParticipants from "./components/ProposalRemoveParticipants";
-import ProposalWhitelistPair from "./components/ProposalWhitelistPair";
 import SelectProposal from "./components/SelectProposal";
 import ShowTxResultProposal from "./components/ShowTxResultProposal";
 import { ModalHeader, Separator, StyledModal } from "./style";
+
+const ProposalAddParticipants = lazy(() => import("./components/ProposalAddParticipants"));
+const ProposalAddVotingParticipants = lazy(() => import("./components/ProposalAddVotingParticipants"));
+const ProposalEditDso = lazy(() => import("./components/ProposalEditDso"));
+const ProposalRemoveParticipants = lazy(() => import("./components/ProposalRemoveParticipants"));
+const ProposalWhitelistPair = lazy(() => import("./components/ProposalWhitelistPair"));
 
 const { Title, Text } = Typography;
 const { Step } = Steps;
