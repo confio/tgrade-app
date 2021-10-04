c236b886a26afc966472431914213ccdf32fe897
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-10-04 22:42:36 +0200

    Move tmarket Provider to main switch

--- src/App/pages/TMarket/routes/index.tsx
+++ src/App/pages/TMarket/routes/index.tsx
@@ -1,27 +1,24 @@
 import { paths } from "App/paths";
 import { Redirect, Route, Switch } from "react-router-dom";
-import TMarketProvider from "service/tmarket";
 import Exchange from "./exchange/";
 import Provide from "./provide/";
 import Withdraw from "./withdraw";
 
 export default function TMarket(): JSX.Element {
   return (
-    <TMarketProvider>
-      <Switch>
-        <Route path={`${paths.tmarket.prefix}${paths.tmarket.exchange.prefix}`}>
-          <Exchange />
-        </Route>
-        <Route path={`${paths.tmarket.prefix}${paths.tmarket.provide.prefix}`}>
-          <Provide />
-        </Route>
-        <Route path={`${paths.tmarket.prefix}${paths.tmarket.withdraw.prefix}`}>
-          <Withdraw />
-        </Route>
-        <Route exact path={paths.tmarket.prefix}>
-          <Redirect to={`${paths.tmarket.prefix}${paths.tmarket.exchange.prefix}`} />
-        </Route>
-      </Switch>
-    </TMarketProvider>
+    <Switch>
+      <Route path={`${paths.tmarket.prefix}${paths.tmarket.exchange.prefix}`}>
+        <Exchange />
+      </Route>
+      <Route path={`${paths.tmarket.prefix}${paths.tmarket.provide.prefix}`}>
+        <Provide />
+      </Route>
+      <Route path={`${paths.tmarket.prefix}${paths.tmarket.withdraw.prefix}`}>
+        <Withdraw />
+      </Route>
+      <Route exact path={paths.tmarket.prefix}>
+        <Redirect to={`${paths.tmarket.prefix}${paths.tmarket.exchange.prefix}`} />
+      </Route>
+    </Switch>
   );
 }
