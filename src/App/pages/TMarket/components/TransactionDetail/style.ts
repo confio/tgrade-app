522b1582af836fbd2b7b0eaee9bb7392e5752f78
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-10-15 14:34:15 +0200

    Tweak TransactionDetail

--- src/App/pages/TMarket/components/TransactionDetail/style.ts
+++ src/App/pages/TMarket/components/TransactionDetail/style.ts
@@ -10,34 +10,37 @@ export const ImageContainer = styled(Row)`
   margin-bottom: 60px;
   justify-content: center;
 `;
+
 export const TitleContainer = styled(Row)`
   margin-bottom: 16px;
   justify-content: center;
+
+  & h1.ant-typography {
+    color: white;
+  }
 `;
+
 export const HorizontalDivider = styled(Divider)`
   margin: var(--s0) 0;
   opacity: 0.5;
 `;
+
 export const RowContainer = styled(Row)`
   justify-content: space-between;
   align-items: center;
   padding: calc(var(--s0) / 2) 0;
 `;
+
 export const Paragraph = styled(Typography.Paragraph)`
   &.ant-typography {
-    color: var(--color-text-1ary);
-    font-size: calc(var(--s0) * 0.85);
+    color: white;
+    font-size: var(--s-1);
   }
 `;
-export const ParagraphStrong = styled(Typography.Paragraph)`
-  &.ant-typography {
-    font-weight: 700;
-    font-size: calc(var(--s0) * 0.85);
-  }
-`;
+
 export const OkButton = styled(Button)`
-  width: 100%;
-  margin: var(--s4) 0;
+  width: 340px;
+  margin: var(--s4) auto;
   border-radius: 50px;
   display: flex;
   justify-content: center;
