cbad2815335a94ca4cb54df8c3619dec78f8fcc9
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-10-15 14:34:54 +0200

    Add result modal to Provide

--- src/App/pages/TMarket/routes/provide/routes/result/style.ts
+++
@@ -1,9 +0,0 @@
-import { Tag } from "antd";
-import styled from "styled-components";
-
-export default styled(Tag)`
-  background: transparent;
-  cursor: pointer;
-  border: none;
-  color: var(--color-primary);
-`;
