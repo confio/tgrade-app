07948b6b0b963f32e9a02f542a050ad6d7e4fc24
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-10-22 19:13:33 +0200

    Tweak ListTokens

--- src/App/pages/TMarket/components/SelecTokenModal/components/ListTokens/style.ts
+++ src/App/pages/TMarket/components/SelecTokenModal/components/ListTokens/style.ts
@@ -1,54 +1,55 @@
-import { Divider, List, Typography } from "antd";
+import { List } from "antd";
 import styled from "styled-components";
 
-export default styled(List)`
-  li {
-    display: flex;
-    flex-direction: column;
+export const TokenListItem = styled(List.Item)`
+  display: flex;
+  align-items: center;
+  gap: var(--s-2);
+`;
+
+export const ContainerLogoNames = styled.div`
+  display: flex;
+  align-items: center;
+  gap: var(--s-2);
+  flex-shrink: 0;
+
+  & img {
+    height: 32px;
   }
-  .token-item {
+`;
+
+export const ContainerNames = styled.div`
+  & div.ant-typography {
+    font-size: var(--s-1);
+
+    &:first-child {
+      font-size: var(--s0);
+      font-weight: 700;
+      color: var(--color-text-1ary);
+    }
+  }
+`;
+
+export const ContainerNumbersPin = styled.div`
+  display: flex;
+  align-items: center;
+  gap: var(--s-2);
+
+  img {
     cursor: pointer;
-    width: 100%;
-    padding: calc(var(--s0) * 0.5) var(--s0);
-    column-gap: 10px;
-    img {
-      width: 50px;
-      height: 50px;
-    }
-    div {
-      h1 {
-        &.ant-typography {
-          font-size: var(--s1);
-        }
-      }
-    }
-    &:hover {
-      background: var(--color-primary);
-      border-radius: 3px;
-      color: var(--bg-body);
-    }
-    &.disabled {
-      opacity: 0.5;
-      cursor: default;
-      &:hover {
-        background: transparent;
-        color: rgba(0, 0, 0, 0.85);
-      }
-    }
+    height: 16px;
   }
 `;
 
-export const Title = styled(Typography.Title)`
-  &.ant-typography {
-    font-size: var(--s0);
-    padding: 0 var(--s0);
-    font-weight: 500;
-    width: 100%;
-    text-aling: start;
-    margin: 5px 0;
-  }
-`;
+export const ContainerNumbers = styled.div`
+  & div.ant-typography {
+    text-align: end;
+    font-size: var(--s-1);
 
-export const StyledDivider = styled(Divider)`
-  margin: 0;
+    &:first-child {
+      font-size: var(--s0);
+      font-weight: 700;
+      color: var(--color-text-1ary);
+    }
+  }
 `;
