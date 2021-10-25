2ed13b8c48d620cd3d87fb0f21fb39320d8f6fb5
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-10-25 10:33:55 +0200

    Tweak download script

--- scripts/tgrade/contracts/download_releases.sh
+++ scripts/tgrade/contracts/download_releases.sh
@@ -2,22 +2,50 @@
 set -o errexit -o nounset -o pipefail
 command -v shellcheck > /dev/null && shellcheck "$0"
 
-if [ $# -ne 1 ]; then
-  echo "Usage: ./download_releases.sh RELEASE_TAG"
+if [ $# -ne 3 ]; then
+  echo "Usage: ./download_releases.sh cwplus_tag tgrade_contracts_tag tfi_tag"
   exit 1
 fi
 
-tag="$1"
+cwplus_tag="$1"
+tgrade_contracts_tag="$2"
+tfi_tag="$3"
+
 # load token from OS keychain when not set via ENV
 GITHUB_API_TOKEN=${GITHUB_API_TOKEN:-"$(security find-generic-password -a "$USER" -s "github_api_key" -w)"}
 
+# delete local contracts and versions
+rm -f *.wasm
 rm -f version.txt
-for contract in tgrade_dso; do
-  list_asset_url="https://api.github.com/repos/confio/tgrade-contracts/releases/tags/${tag}"
-  # get url for artifact with name==$artifact
+
+CWPLUS="cw20_base"
+for contract in $CWPLUS; do
+  list_asset_url="https://api.github.com/repos/CosmWasm/cw-plus/releases/tags/${cwplus_tag}"
+  # get url for artifact with name==${contract}.wasm
+  artifact_url=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_API_TOKEN" "${list_asset_url}" | jq -r ".assets[] | select(.name==\"${contract}.wasm\") | .url")
+  # download the artifact
+  curl -LJO -H 'Accept: application/octet-stream' -H "Authorization: token $GITHUB_API_TOKEN" "$artifact_url"
+done
+echo "CWPLUS_TAG=$cwplus_tag" >> version.txt
+
+TGRADE_CONTRACTS="tgrade_dso"
+for contract in $TGRADE_CONTRACTS; do
+  list_asset_url="https://api.github.com/repos/confio/tgrade-contracts/releases/tags/${tgrade_contracts_tag}"
+  # get url for artifact with name==${contract}.wasm
   asset_url=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_API_TOKEN" "${list_asset_url}" | jq -r ".assets[] | select(.name==\"${contract}.wasm\") | .url")
   # download the artifact
   curl -LJO -H 'Accept: application/octet-stream' -H "Authorization: token $GITHUB_API_TOKEN" "$asset_url"
 done
+echo "TGRADE_CONTRACTS_TAG=$tgrade_contracts_tag" >> version.txt
 
-echo "$tag" >version.txt
+TFI="dso_token tfi_factory tfi_pair"
+for contract in $TFI; do
+  list_asset_url="https://api.github.com/repos/confio/tfi/releases/tags/${tfi_tag}"
+  # get url for artifact with name==${contract}.wasm
+  asset_url=$(curl -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_API_TOKEN" "${list_asset_url}" | jq -r ".assets[] | select(.name==\"${contract}.wasm\") | .url")
+  # download the artifact
+  curl -LJO -H 'Accept: application/octet-stream' -H "Authorization: token $GITHUB_API_TOKEN" "$asset_url"
+done
+echo "TFI_TAG=$tfi_tag" >> version.txt
+
+echo "Done. All artifacts downloaded and versions updated."
