cf9177612cba10541d1da8e164f9614ffc188572
hannuprod <hannubit@gmail.com>
2021-10-26 16:23:33 +0200

    fixed test imports

--- src/App/components/AddDsoModal/components/FormDsoAddExisting/index.test.tsx
+++ src/App/components/AddDsoModal/components/FormDsoAddExisting/index.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+
 import FormDsoAddExisting from ".";
 
 it("renders and submits FormDsoAddExisting", async () => {
