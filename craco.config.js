0537bd27eeda2d0ba5c0a5d0109286f782aefae7
hannuprod <hannubit@gmail.com>
2021-11-01 15:33:27 +0100

    wip:memory fix

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   plugins: [{ plugin: new MiniCssExtractPlugin() }, { plugin: require("craco-antd") }],
@@ -9,6 +11,14 @@ module.exports = {
         test: /\.css$/i,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+      }),
     ],
   },
 };
