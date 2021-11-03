55493cae1646088365faf9e9cec4d764e86a7b9c
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-11-03 17:32:39 +0100

    Add missing quotes

--- scripts/proto/get-proto.sh
+++ scripts/proto/get-proto.sh
@@ -2,4 +2,4 @@
 # TODO: get automatically from tgrade repo
 # Create a "tgrade" folder with "confio" and "third_party" subfolders.
 # "confio" contents come from: https://github.com/confio/tgrade/tree/main/proto/confio
-# third_party contents come from: https://github.com/confio/tgrade/tree/main/third_party
+# "third_party" contents come from: https://github.com/confio/tgrade/tree/main/third_party
