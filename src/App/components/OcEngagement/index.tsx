69ad849ce22e70ff8ed9896efe56694cfa3be8d0
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-11-08 18:12:15 +0100

    Add OcEngagement component

---
+++ src/App/components/OcEngagement/index.tsx
@@ -0,0 +1,59 @@
+import { Typography } from "antd";
+import { useEffect, useState } from "react";
+import { useError, useSdk } from "service";
+import { EngagementContractQuerier, FormattedHalflifeInfo } from "utils/poeEngagement";
+
+import { EgDataStack, EngagementStack } from "./style";
+
+const { Title, Text } = Typography;
+
+export default function OcEngagement(): JSX.Element {
+  const { handleError } = useError();
+  const {
+    sdkState: { client, config },
+  } = useSdk();
+
+  const [egPoints, setEgPoints] = useState(0);
+  const [halflifeInfo, setHalflifeInfo] = useState<FormattedHalflifeInfo>();
+
+  useEffect(() => {
+    (async function updateNumMembers() {
+      if (!client) return;
+
+      try {
+        const egContract = new EngagementContractQuerier(config, client);
+
+        const egPoints = await egContract.getTotalEngagementPoints();
+        setEgPoints(egPoints);
+
+        const halflifeInfo = await egContract.getFormattedHalflifeInfo();
+        setHalflifeInfo(halflifeInfo);
+      } catch (error) {
+        if (!(error instanceof Error)) return;
+        handleError(error);
+      }
+    })();
+  }, [client, config, handleError]);
+
+  return (
+    <EngagementStack>
+      <Title level={2}>Engagement</Title>
+      <EgDataStack gap="s-4">
+        <Text>Total engagement points:</Text>
+        <Text>{egPoints}</Text>
+      </EgDataStack>
+      <EgDataStack gap="s-4">
+        <Text>Halflife duration:</Text>
+        <Text>{halflifeInfo?.halflifeDuration}</Text>
+      </EgDataStack>
+      <EgDataStack gap="s-4">
+        <Text>Last halflife event:</Text>
+        <Text>{`${halflifeInfo?.lastHalflifeDate.toLocaleDateString()} ${halflifeInfo?.lastHalflifeDate.toLocaleTimeString()}`}</Text>
+      </EgDataStack>
+      <EgDataStack gap="s-4">
+        <Text>Next halflife event:</Text>
+        <Text>{`${halflifeInfo?.nextHalflifeDate.toLocaleDateString()} ${halflifeInfo?.nextHalflifeDate.toLocaleTimeString()}`}</Text>
+      </EgDataStack>
+    </EngagementStack>
+  );
+}
