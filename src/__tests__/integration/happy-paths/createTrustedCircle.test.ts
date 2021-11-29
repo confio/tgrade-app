8de6ff18ad6c29d372672b3da7a1796aa7fa1ccf
Abel Fernández <abel.fernandez.nandin@gmail.com>
2021-11-29 17:16:38 +0100

    Add missing tests (#229)
    * Adds missing tests for trusted circle
    
    * Adds missing tests for amm
    
    * Increase timeout

--- src/__tests__/integration/happy-paths/createTrustedCircle.test.ts
+++ src/__tests__/integration/happy-paths/createTrustedCircle.test.ts
@@ -3,7 +3,7 @@ import { config } from "config/network";
 import { DsoContract } from "utils/dso";
 import { createSigningClient, loadOrCreateWallet } from "utils/sdk";
 
-it("creates a Trusted Circle", async () => {
+it("creates a Trusted Circle with a member", async () => {
   const signer = await loadOrCreateWallet(config);
   const signingClient = await createSigningClient(config, signer);
   const address = (await signer.getAccounts())[0].address;
@@ -35,4 +35,50 @@ it("creates a Trusted Circle", async () => {
   );
 
   expect(contractAddress.startsWith(config.addressPrefix)).toBeTruthy();
-});
+}, 30000);
+
+it("creates a Trusted Circle and adds a member with a proposal", async () => {
+  const signer = await loadOrCreateWallet(config);
+  const signingClient = await createSigningClient(config, signer);
+  const address = (await signer.getAccounts())[0].address;
+
+  const faucetClient = new FaucetClient(config.faucetUrl);
+  await faucetClient.credit(address, config.faucetTokens?.[0] ?? config.feeToken);
+
+  const dsoName = "Trusted Circle 2";
+  const escrowAmount = "1000000";
+  const votingDuration = "19";
+  const quorum = "30";
+  const threshold = "51";
+  const members: readonly string[] = [];
+  const allowEndEarly = true;
+
+  const contractAddress = await DsoContract.createDso(
+    signingClient,
+    config.codeIds?.tgradeDso?.[0] ?? 0,
+    address,
+    dsoName,
+    escrowAmount,
+    votingDuration,
+    quorum,
+    threshold,
+    members,
+    allowEndEarly,
+    [{ denom: config.feeToken, amount: escrowAmount }],
+    config.gasPrice,
+  );
+
+  const comment = "Add new member";
+  const memberToAdd = "tgrade1uuy629yfuw2mf383t33x0jk2qwtf6rvv4dhmxs";
+
+  const dsoContract = new DsoContract(contractAddress, signingClient, config.gasPrice);
+  await dsoContract.propose(signingClient, config.factoryAddress, address, comment, {
+    add_remove_non_voting_members: {
+      remove: [],
+      add: [memberToAdd],
+    },
+  });
+
+  const txHash = await dsoContract.executeProposal(address, 1);
+  expect(txHash).toBeTruthy();
+}, 30000);
