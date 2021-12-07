328afe96e3316bc1b0cf49475daf17e91499e923
hannuprod <hannubit@gmail.com>
2021-12-07 10:39:58 +0100

    changed plugin order

--- craco.config.js
+++ craco.config.js
@@ -3,12 +3,12 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   plugins: [
+    { plugin: require("craco-antd") },
     {
       plugin: new MiniCssExtractPlugin({
         ignoreOrder: true,
       }),
     },
-    { plugin: require("craco-antd") },
   ],
 
   module: {
