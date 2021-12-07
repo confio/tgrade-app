4aea8c7987f9e43c4b370af0f28a77a600968482
hannuprod <hannubit@gmail.com>
2021-12-07 11:30:03 +0100

    testing alternative configuration

--- craco.config.js
+++ craco.config.js
@@ -1,22 +1,31 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+const CracoAntDesignPlugin = require("craco-antd");
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  plugins: [
-    { plugin: require("craco-antd") },
-    {
-      plugin: new MiniCssExtractPlugin({
-        ignoreOrder: true,
-      }),
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig = {
+        ...webpackConfig,
+        plugins: [
+          ...webpackConfig.plugins.filter((element) => {
+            if (element.options) {
+              // eslint-disable-next-line no-prototype-builtins
+              return !element.options.hasOwnProperty("ignoreOrder");
+            }
+            return true;
+          }),
+          new MiniCssExtractPlugin({
+            filename: "static/css/[name].[contenthash:8].css",
+            moduleFilename: this.moduleFilename,
+            ignoreOrder: true,
+            chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+          }),
+        ],
+      };
+      return webpackConfig;
     },
-  ],
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ],
   },
+  plugins: [{ plugin: CracoAntDesignPlugin }],
 };
