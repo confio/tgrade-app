1a6c1bc1fc44c23dfb9cdbcfd9885baa5e196788
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-01-13 15:18:57 +0100

    Add ProposalUpdateConsensusBlockParams create proposal type

---
+++ src/App/components/ValidatorCreateProposalModal/components/ProposalUpdateConsensusBlockParams/components/FormUpdateConsensusBlockParams/index.tsx
@@ -0,0 +1,83 @@
+import BackButtonOrLink from "App/components/BackButtonOrLink";
+import Button from "App/components/Button";
+import Field from "App/components/Field";
+import Stack from "App/components/Stack/style";
+import { Formik } from "formik";
+import { Form } from "formik-antd";
+import { getFormItemName } from "utils/forms";
+import * as Yup from "yup";
+
+import { ButtonGroup, Separator } from "./style";
+
+const maxBytesLabel = "Maximum bytes";
+const maxGasLabel = "Maximum gas";
+const commentLabel = "Comment";
+
+const validationSchema = Yup.object().shape({
+  [getFormItemName(maxBytesLabel)]: Yup.number().typeError("Max bytes must be numeric"),
+  [getFormItemName(maxGasLabel)]: Yup.number().typeError("Max gas must be numeric"),
+  [getFormItemName(commentLabel)]: Yup.number().typeError("Comment must be numeric"),
+});
+
+export interface FormUpdateConsensusBlockParamsValues {
+  readonly maxBytes: string;
+  readonly maxGas: string;
+  readonly comment: string;
+}
+
+interface FormUpdateConsensusBlockParamsProps extends FormUpdateConsensusBlockParamsValues {
+  readonly goBack: () => void;
+  readonly handleSubmit: (values: FormUpdateConsensusBlockParamsValues) => void;
+}
+
+export default function FormUpdateConsensusBlockParams({
+  maxBytes,
+  maxGas,
+  comment,
+  goBack,
+  handleSubmit,
+}: FormUpdateConsensusBlockParamsProps): JSX.Element {
+  return (
+    <Formik
+      initialValues={{
+        [getFormItemName(maxBytesLabel)]: maxBytes,
+        [getFormItemName(maxGasLabel)]: maxGas,
+        [getFormItemName(commentLabel)]: comment,
+      }}
+      enableReinitialize
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        handleSubmit({
+          maxBytes: values[getFormItemName(maxBytesLabel)],
+          maxGas: values[getFormItemName(maxGasLabel)],
+          comment: values[getFormItemName(commentLabel)],
+        });
+      }}
+    >
+      {({ isValid, values, submitForm }) => (
+        <>
+          <Form>
+            <Stack gap="s1">
+              <Field label={maxBytesLabel} placeholder="Enter max bytes" optional />
+              <Field label={maxGasLabel} placeholder="Enter max gas" optional />
+              <Field label={commentLabel} placeholder="Enter comment" optional />
+              <Separator />
+              <ButtonGroup>
+                <BackButtonOrLink onClick={() => goBack()} text="Back" />
+                <Button
+                  disabled={
+                    !isValid ||
+                    !(values[getFormItemName(maxBytesLabel)] || values[getFormItemName(maxGasLabel)])
+                  }
+                  onClick={() => submitForm()}
+                >
+                  <div>Create proposal</div>
+                </Button>
+              </ButtonGroup>
+            </Stack>
+          </Form>
+        </>
+      )}
+    </Formik>
+  );
+}
