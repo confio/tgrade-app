8557aa1c37ba68ffdc0aa479a93511aa5ba34a23
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-01-13 15:17:50 +0100

    Add ProposalCancelUpgrade create proposal type

---
+++ src/App/components/ValidatorCreateProposalModal/components/ProposalCancelUpgrade/components/ConfirmationCancelUpgrade/style.ts
@@ -0,0 +1,37 @@
+import { Typography } from "antd";
+import styled from "styled-components";
+
+const { Text } = Typography;
+
+export const TextComment = styled(Text)`
+  && {
+    color: var(--color-text-1ary);
+  }
+`;
+
+export const Separator = styled.hr`
+  margin: 0 -20px 0 -20px;
+  border: none;
+  border-top: 1px solid var(--color-input-border);
+`;
+
+export const ButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  & > *:first-child {
+    margin-right: var(--s0);
+  }
+`;
+
+export const FeeGroup = styled.div`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+
+  & > *:first-child {
+    margin-right: var(--s0);
+  }
+`;
