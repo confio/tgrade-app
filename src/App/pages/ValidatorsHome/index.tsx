8600fe9066edb20ea4b36bfd9b1d4d069a949374
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-01-13 15:03:01 +0100

    Enable ValidatorProposals. Add welcome

--- src/App/pages/ValidatorsHome/index.tsx
+++ src/App/pages/ValidatorsHome/index.tsx
@@ -5,18 +5,19 @@ import { lazy } from "react";
 import { TextStack } from "./style";
 
 const ValidatorOverview = lazy(() => import("App/components/ValidatorOverview"));
-/* const ValidatorProposals = lazy(() => import("App/components/ValidatorProposals")); */
-const { Title } = Typography;
+const ValidatorProposals = lazy(() => import("App/components/ValidatorProposals"));
+
+const { Title, Text } = Typography;
 
 export default function ValidatorsHome(): JSX.Element | null {
   return (
     <PageLayout maxwidth="100%" centered="false">
       <TextStack>
         <Title>Validators</Title>
+        <Text>Welcome to Mission Control</Text>
       </TextStack>
-      {/* TODO readd in validator voting PR:
-       <ValidatorProposals /> */}
       <ValidatorOverview />
+      <ValidatorProposals />
     </PageLayout>
   );
 }
