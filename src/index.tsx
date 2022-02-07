6f1ae267f32b055963d4b793b8105a16cc24c7ce
Evgenii Bazhanov <evg.bazhanov@yhaoo.com>
2022-02-07 07:51:09 +0100

    fix console warning -> findDOMNode is deprecated in StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import "./index.css";
 import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import { credentials } from "config/credentials";
-import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
@@ -12,18 +11,15 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 Sentry.init({
   dsn: credentials.sentry.dsn,
-  integrations: [new Integrations.BrowserTracing()],
-  // Sample rate to determine trace sampling (percentage of traces sent)
+  integrations: [new Integrations.BrowserTracing()], // Sample rate to determine trace sampling (percentage of traces sent)
   // 0.0 = send no traces 1.0 = send all traces
   tracesSampleRate: 1.0,
 });
 
 ReactDOM.render(
-  <StrictMode>
-    <Sentry.ErrorBoundary fallback={<div>An error has occurred</div>}>
-      <App />
-    </Sentry.ErrorBoundary>
-  </StrictMode>,
+  <Sentry.ErrorBoundary fallback={<div>An error has occurred</div>}>
+    <App />
+  </Sentry.ErrorBoundary>,
   document.getElementById("root"),
 );
 
