4ebd7756d3c8c9a21bc79faec4725f91c1346c99
Zhenja <evg.bazhanov@yahoo.com>
2022-02-09 16:28:08 +0100

    some improvements

--- src/__tests__/integration/happy-paths/ocContract.test.ts
+++ src/__tests__/integration/happy-paths/ocContract.test.ts
@@ -1,14 +1,15 @@
-import { config } from "config/network";
 import { createSigningClient } from "utils/sdk";
 import { OcContract } from "../../../utils/oversightCommunity";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { makeCosmoshubPath } from "@cosmjs/stargate";
+import { config } from "config/network";
+import { Random } from "@cosmjs/crypto";
+import { Bech32 } from "@cosmjs/encoding";
 
-const mnemonic = process.env.SECRET_MNEMONIC || "";
+const mnemonic = process.env.SECRET_MNEMONIC || ""; // to run locally use real mnemonic instead
+const addressPrefix = "tgrade";
 
-it("creates proposal for Add Oversight Community member", async () => {
-  const addressPrefix = "tgrade";
-
+it("creates OC proposal with Oversight Community member", async () => {
   const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
     hdPaths: [makeCosmoshubPath(0)],
     prefix: addressPrefix,
@@ -19,17 +20,21 @@ it("creates proposal for Add Oversight Community member", async () => {
   const { address } = (await wallet.getAccounts())[0];
 
   const comment = "Add new member";
-  const memberToAdd = "tgrade1kalzk5cvq5yu6f5u73k7r905yw52sawckddsc3";
+  const memberToAdd = makeRandomAddress();
 
   const OcCommunity = new OcContract(config, signingClient);
 
-  await OcCommunity.propose(address, comment, {
+  const oC = await OcCommunity.propose(address, comment, {
     add_voting_members: {
       voters: [memberToAdd],
     },
   });
 
-  const txHash = await OcCommunity.executeProposal(address, "tc1");
+  const txHash = await OcCommunity.executeProposal(address, oC.proposalId);
 
   expect(txHash).toBeTruthy();
 }, 10000);
+
+function makeRandomAddress(): string {
+  return Bech32.encode("tgrade", Random.getBytes(20));
+}
