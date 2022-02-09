dda99350b0ea994050988410bd80a4f4810c875b
Zhenja <evg.bazhanov@yahoo.com>
2022-02-09 12:17:22 +0100

    next attempt to store mnemonic in circleci var

--- src/__tests__/integration/happy-paths/ocContract.test.ts
+++ src/__tests__/integration/happy-paths/ocContract.test.ts
@@ -4,34 +4,32 @@ import { OcContract } from "../../../utils/oversightCommunity";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { makeCosmoshubPath } from "@cosmjs/stargate";
 
-const mnemonic = process.env.SECRET_MNEMONIC;
+const mnemonic = process.env.SECRET_MNEMONIC || "";
 
-describe("create a signing client using the mnemonic", async () => {
-  it("creates proposal for Add Oversight Community member", async () => {
-    const addressPrefix = "tgrade";
+it("creates proposal for Add Oversight Community member", async () => {
+  const addressPrefix = "tgrade";
 
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
-      hdPaths: [makeCosmoshubPath(0)],
-      prefix: addressPrefix,
-    });
+  const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    hdPaths: [makeCosmoshubPath(0)],
+    prefix: addressPrefix,
+  });
 
-    const signingClient = await createSigningClient(config, wallet);
+  const signingClient = await createSigningClient(config, wallet);
 
-    const { address } = (await wallet.getAccounts())[0];
+  const { address } = (await wallet.getAccounts())[0];
 
-    const comment = "Add new member";
-    const memberToAdd = "tgrade1kalzk5cvq5yu6f5u73k7r905yw52sawckddsc3";
+  const comment = "Add new member";
+  const memberToAdd = "tgrade1kalzk5cvq5yu6f5u73k7r905yw52sawckddsc3";
 
-    const OcCommunity = new OcContract(config, signingClient);
+  const OcCommunity = new OcContract(config, signingClient);
 
-    await OcCommunity.propose(address, comment, {
-      add_voting_members: {
-        voters: [memberToAdd],
-      },
-    });
+  await OcCommunity.propose(address, comment, {
+    add_voting_members: {
+      voters: [memberToAdd],
+    },
+  });
 
-    const txHash = await OcCommunity.executeProposal(address, "tc1");
+  const txHash = await OcCommunity.executeProposal(address, "tc1");
 
-    expect(txHash).toBeTruthy();
-  }, 10000);
-});
+  expect(txHash).toBeTruthy();
+}, 10000);
