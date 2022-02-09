287f1d74fa37c76c7fd753b946541cdf7431cde7
Alex Peters <alpe@users.noreply.github.com>
2022-02-09 15:36:42 +0100

    Start tgrade v0.6.0-beta1 upgrade

---
+++ scripts/tgrade/start_debug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -ox errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
+# Please keep this in sync with the Ports overview in HACKING.md
+TENDERMINT_PORT_GUEST="26657"
+TENDERMINT_PORT_HOST="26657"
+LCD_API_PORT_GUEST="1317"
+LCD_API_PORT_HOST="1317"
+
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+# shellcheck source=./env
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR"/env
+
+# Use a fresh volume for every start
+docker volume rm -f tgrade_data
+# docker pull "$REPOSITORY:$VERSION"
+
+# This starts up tgrade
+docker run --rm \
+  --name "$CONTAINER_NAME" \
+  -p "$TENDERMINT_PORT_HOST":"$TENDERMINT_PORT_GUEST" \
+  -p "$LCD_API_PORT_HOST":"$LCD_API_PORT_GUEST" \
+  --mount type=bind,source="$SCRIPT_DIR/template",target=/template \
+  --mount type=volume,source=tgrade_data,target=/root \
+  "$REPOSITORY:$VERSION" \
+  /bin/sh -c "cp -r /template/node0/tgrade /root; tgrade start --trace --home=/root/tgrade 2>&1"
