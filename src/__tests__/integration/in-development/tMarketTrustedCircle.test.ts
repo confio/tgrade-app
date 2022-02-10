926be6f8c1a57cf1b29f84e6d62a8c80e9383e54
Zhenja <evg.bazhanov@yahoo.com>
2022-02-10 16:55:22 +0100

    Add design for "Integration tests" without implementation (#346)

---
+++ src/__tests__/integration/in-development/tMarketTrustedCircle.test.ts
@@ -0,0 +1,16 @@
+xdescribe("T-Market and Trusted Circle", () => {
+  /**
+   * Make sure that if a token is associated to a Trusted Circle,
+   * you can only operate with it after whitelisting
+   * the pair with a proposal of the Trusted Circle, else it will fail.
+   */
+  it("pair with a proposal of the Trusted Circle", () => {
+    //TODO
+  });
+
+  it("fail to pair with a proposal of the Trusted Circle", () => {
+    //TODO
+  });
+});
+
+export {};
---
+++ src/__tests__/integration/in-development/wallet.test.ts
@@ -0,0 +1,13 @@
+xdescribe("Wallet", () => {
+  it("Send native token", () => {
+    //TODO
+  });
+  it("Send CW20 token", () => {
+    //TODO
+  });
+  it("Send Trusted token", () => {
+    //TODO
+  });
+});
+
+export {};
