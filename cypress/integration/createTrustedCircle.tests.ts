112ee8865ae937aadf21da24a606e15bee167423
Zhenja <evg.bazhanov@yahoo.com>
2022-02-16 09:36:30 +0100

    attempt #28

--- cypress/integration/createTrustedCircle.tests.ts
+++ cypress/integration/createTrustedCircle.tests.ts
@@ -8,8 +8,8 @@ describe("Trusted Circle", () => {
       cy.findByText(/Add Trusted Circle/i).click();
       cy.findByText(/Create Trusted Circle/i).click();
       cy.findByPlaceholderText(/Enter Trusted Circle name/i)
-        .type("Trusted Circle Test #1")
-        .should("have.value", "Trusted Circle Test #1");
+        .type("Trusted Circle Test #2")
+        .should("have.value", "Trusted Circle Test #2");
       cy.get(".ant-modal-content header h1").should("have.text", "Start Trusted Circle");
       cy.get(".ant-steps-item-active .ant-steps-item-icon span").should("have.text", "1");
       cy.findByRole("button", { name: /Next/i }).click();
@@ -19,11 +19,14 @@ describe("Trusted Circle", () => {
 
       cy.get(".ant-steps-item-active .ant-steps-item-icon span").should("have.text", "3");
       cy.findByRole("button", { name: /Connect wallet/i }).click();
-      cy.wait(5000); //should be improved
+
       cy.contains("Web wallet (demo)").click();
+      cy.findByRole("button", { name: /Try again/i }).should("not.exist");
       cy.wait(5000); //should be improved
+
+      cy.get(".ant-steps-item-active .ant-steps-item-icon span").should("have.text", "3");
       cy.findByRole("button", { name: /Sign transaction and pay escrow/i }).click();
-      cy.wait(20000); //should be improved
+
       cy.findByRole("button", { name: /Go to Trusted Circle details/i }).click();
     });
     it("show that trusted circle is created", () => {
