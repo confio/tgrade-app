c3f9a761ece8b1c5d645698358235492b1960286
Zhenja <evg.bazhanov@yahoo.com>
2022-02-16 13:26:34 +0100

    attempt #29

--- cypress/integration/createTrustedCircle.tests.ts
+++ cypress/integration/createTrustedCircle.tests.ts
@@ -8,8 +8,8 @@ describe("Trusted Circle", () => {
       cy.findByText(/Add Trusted Circle/i).click();
       cy.findByText(/Create Trusted Circle/i).click();
       cy.findByPlaceholderText(/Enter Trusted Circle name/i)
-        .type("Trusted Circle Test #2")
-        .should("have.value", "Trusted Circle Test #2");
+        .type("Trusted Circle Test #4")
+        .should("have.value", "Trusted Circle Test #4");
       cy.get(".ant-modal-content header h1").should("have.text", "Start Trusted Circle");
       cy.get(".ant-steps-item-active .ant-steps-item-icon span").should("have.text", "1");
       cy.findByRole("button", { name: /Next/i }).click();
@@ -20,17 +20,17 @@ describe("Trusted Circle", () => {
       cy.get(".ant-steps-item-active .ant-steps-item-icon span").should("have.text", "3");
       cy.findByRole("button", { name: /Connect wallet/i }).click();
 
-      cy.contains("Web wallet (demo)").click();
+      cy.findByText("Web wallet (demo)").click();
       cy.findByRole("button", { name: /Try again/i }).should("not.exist");
-      cy.wait(5000); //should be improved
 
       cy.get(".ant-steps-item-active .ant-steps-item-icon span").should("have.text", "3");
+      cy.wait(1500); // do not know what happens in 1,5s but if there is no wait it does not click on the button
       cy.findByRole("button", { name: /Sign transaction and pay escrow/i }).click();
 
       cy.findByRole("button", { name: /Go to Trusted Circle details/i }).click();
     });
     it("show that trusted circle is created", () => {
-      cy.contains("Trusted Circle Test #1").should("be.visible");
+      cy.get("h1.ant-typography").should("have.text", "Trusted Circle Test #4");
     });
   });
 });
