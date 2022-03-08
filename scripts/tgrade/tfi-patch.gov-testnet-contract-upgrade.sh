b9761ae6ab696a2d3f597dca369e6d78ab7ac943
Mauro Lacy <mauro@lacy.com.es>
2022-03-08 10:53:53 +0100

    Generalize tgrade and contract upgrade scripts to multiple testnets

--- scripts/tgrade/tfi-patch.gov-testnet-contract-upgrade.sh
+++ scripts/tgrade/tfi-patch.gov-testnet-contract-upgrade.sh
@@ -3,19 +3,24 @@ set -o errexit -o nounset -o pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-#------------------
-home_dir=t"$HOME/.tgrade"
+# Link this to testnet's env file
+source ./env.sh
 
-# Keys: Use `tgrade key add --recover` to add at least two validators
-key="validator-2"
-otherKey="validator-1"
+echo "Tgrade home: $TGRADE_HOME"
+echo "Chain id: $chainId"
+echo "Nodel url: $nodeUrl"
+echo "Correct? [y/N]:"
+read A
+[ "$A" != "y" ] && echo "Abort." && exit 1
 
-# Data from https://confio.slab.com/posts/tgrade-internal-9-d7azqque
-chainId="tgrade-internal-9"
-nodeUrl="https://rpc.internal-9.tgrade.io:443"
+# Keys for voting / execution (must be validators)
+key="validator1-$chainSuffix"
+otherKey1="validator2-$chainSuffix"
+otherKey2="validator3-$chainSuffix"
+
 factoryCodeId=11
 
-factoryContractAddress=$(tgrade q wasm list-contract-by-code $factoryCodeId --node=$nodeUrl -o json  | jq -r  '.contracts[0]')
+factoryContractAddress=$(tgrade q wasm list-contract-by-code $factoryCodeId --node=$nodeUrl -o json | jq -r '.contracts[0]')
 
 echo "--------------------------------------------------------------------------------------------"
 echo " "
@@ -33,26 +38,24 @@ echo "* Code id: $codeID"
 title="Migrate tfi factory contract to code id $codeID"
 description="Migrate tfi factory contract with address '$factoryContractAddress' to code id '$codeID'"
 text_proposal=$(cat <<EOF
-{"propose": {"title": "$title", "description": "description", "proposal": {"text": {}} }}
-
+{"propose": {"title": "$title", "description": "$description", "proposal": {"text": {}} }}
 EOF
 )
 
 # Prepare migration proposal message
 upgrade_proposal=$(cat <<EOF
 {"propose": {"title": "$title", "description": "$description", "proposal": {"migrate_contract": {"contract":"$factoryContractAddress", "code_id": $codeID, "migrate_msg": "$(echo -n '{}' | base64)"}} }}
-
 EOF
 )
 
 valVotingContractAddr=$(tgrade q poe contract-address VALIDATOR_VOTING -o json --node="$nodeUrl" | jq -re ".address")
 
+echo "Validator voting address: $valVotingContractAddr"
 echo "Text proposal: $text_proposal"
 echo "tfi-factory upgrade proposal: $upgrade_proposal"
-echo "validator voting address: $valVotingContractAddr"
 
 echo
-echo "Proceed? [y/N/t]: "
+echo "Proceed? [y(es)/N(o)/t(ext)]: "
 read A
 [ "$A" = "y" -o "$A" = "t" ] || exit
 
@@ -68,36 +71,44 @@ fi
 echo "proposal: $msg"
 
 rsp=$(tgrade tx wasm execute "$valVotingContractAddr" "$msg" \
-      --from $key --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json)
+  --from $key --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json)
 
 echo $rsp
 proposal_id=$(echo "$rsp" | jq -er '.logs[0].events[-1].attributes[3].value')
 
-msg=$(cat << EOF
+# Vote
+msg=$(cat <<EOF
 {"vote": {"proposal_id": $proposal_id, "vote": "yes"}}
 EOF
 )
 echo "-----------------------"
 echo "Vote proposal: $msg"
 echo "-----------------------"
-tgrade tx wasm execute  \
+echo "$otherKey1 votes yes:"
+tgrade tx wasm execute \
   "$valVotingContractAddr" "$msg" \
-  --from $otherKey --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl"  -b block -o json
-msg=$(cat << EOF
-{"execute": {"proposal_id": $proposal_id}}
-EOF
-)
+  --from $otherKey1 --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json
+echo "$otherKey2 votes yes:"
+tgrade tx wasm execute \
+  "$valVotingContractAddr" "$msg" \
+  --from $otherKey2 --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json
 
 # Check if passed
 proposal_status=$(tgrade query wasm contract-state smart $valVotingContractAddr "{\"proposal\": { \"proposal_id\": $proposal_id }}" --node $nodeUrl -o json | jq -er .data.status)
 echo "Proposal $proposal_id status: $proposal_status"
+[ "$proposal_status" != "passed" ] && echo "Proposal $proposal_id not passed" && exit 1
 
+# Execute
+msg=$(cat <<EOF
+{"execute": {"proposal_id": $proposal_id}}
+EOF
+)
 echo "-----------------------"
 echo "Execute proposal: $msg"
 echo "-----------------------"
-tgrade tx wasm execute  \
+tgrade tx wasm execute \
   "$valVotingContractAddr" "$msg" \
-  --from $key --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl"  -b block -o json
+  --from $key --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json
 
 echo "--------------------------------------------------------------------------------------------"
 echo "## TFI factory migration completed"
@@ -105,7 +116,8 @@ echo "--------------------------------------------------------------------------
 rsp=$(tgrade q wasm contract $factoryContractAddress -o json --node="$nodeUrl")
 echo "$rsp"
 newCodeID=$(echo "$rsp" | jq -re ".contract_info.code_id")
-if [ "$newCodeID" != "$codeID" ]; then
-   echo "Unexpected code id: $newCodeID";
-   exit 1;
+if [ "$newCodeID" != "$codeID" ]
+then
+  echo "Unexpected code id: $newCodeID"
+  exit 1
 fi
--- scripts/tgrade/tfi-patch.gov-testnet-upgrade.sh
+++ scripts/tgrade/tfi-patch.gov-testnet-upgrade.sh
@@ -3,16 +3,21 @@ set -o errexit -o nounset -o pipefail
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-#------------------
-home_dir=t"$HOME/.tgrade"
+# Link this to testnet's env file
+source ./env.sh
 
-# Keys: Use `tgrade key add --recover` to add at least two validators
-key="validator-2"
-otherKey="validator-1"
+echo "Tgrade home: $TGRADE_HOME"
+echo "Chain id: $chainId"
+echo "Nodel url: $nodeUrl"
+echo "Correct? [y/N]:"
+read A
+[ "$A" != "y" ] && echo "Abort." && exit 1
 
-# Data from https://confio.slab.com/posts/tgrade-internal-9-d7azqque
-chainId="tgrade-internal-9"
-nodeUrl="https://rpc.internal-9.tgrade.io:443"
+# Keys for voting / execution (must be validators)
+key="validator1-$chainSuffix"
+otherKey1="validator2-$chainSuffix"
+otherKey2="validator3-$chainSuffix"
+
 factoryCodeId=11
 
 NEW_VERSION="v0.7.0"
@@ -24,7 +29,7 @@ currentHeight=$(tgrade status --node $nodeUrl | jq -re ".SyncInfo.latest_block_h
 upgradeHeight=$(echo "scale=0; ($currentHeight + $UPGRADE_HEIGHT_OFFSET * 1.5) / 1000 * 1000" | bc -l)
 haltHeight=$[upgradeHeight - 1]
 
-factoryContractAddress=$(tgrade q wasm list-contract-by-code $factoryCodeId --node=$nodeUrl -o json  | jq -r  '.contracts[0]')
+factoryContractAddress=$(tgrade q wasm list-contract-by-code $factoryCodeId --node=$nodeUrl -o json | jq -r '.contracts[0]')
 
 echo "--------------------------------------------------------------------------------------------"
 echo " "
@@ -39,24 +44,25 @@ echo "Upgrade height: $upgradeHeight"
 echo "Halt height   : $haltHeight"
 
 # Try with a text proposal first
-text_proposal=$(cat << EOF
+text_proposal=$(cat <<EOF
 {"propose": {"title": "Test", "description": "Upgrade to $NEW_VERSION pre-test using text", "proposal": {"text": {}} }}
 EOF
 )
 
-upgrade_proposal=$(cat << EOF
+upgrade_proposal=$(cat <<EOF
 {"propose": {"title": "Upgrade", "description": "Upgrade to $NEW_VERSION", "proposal": {"register_upgrade": {"name":"$UPGRADE_NAME", "height": $upgradeHeight, "info": "$factoryContractAddress"}} }}
 EOF
 )
 
 valVotingContractAddr=$(tgrade q poe contract-address VALIDATOR_VOTING -o json --node="$nodeUrl" | jq -re ".address")
 
+echo "Validator voting address: $valVotingContractAddr"
+
 echo "Text proposal: $text_proposal"
 echo "Add new upgrade proposal: $upgrade_proposal"
-echo "validator voting address: $valVotingContractAddr"
 
 echo
-echo "Proceed? [y/N/t]: "
+echo "Proceed? [y(es)/N(o)/t(ext)]: "
 read A
 [ "$A" = "y" -o "$A" = "t" ] || exit
 
@@ -77,33 +83,41 @@ rsp=$(tgrade tx wasm execute "$valVotingContractAddr" "$msg" \
 echo $rsp
 proposal_id=$(echo "$rsp" | jq -er '.logs[0].events[-1].attributes[3].value')
 
-msg=$(cat << EOF
+msg=$(cat <<EOF
 {"vote": {"proposal_id": $proposal_id, "vote": "yes"}}
 EOF
 )
 echo "-----------------------"
 echo "Vote proposal: $msg"
 echo "-----------------------"
-tgrade tx wasm execute  \
+echo "$otherKey1 votes yes:"
+tgrade tx wasm execute \
   "$valVotingContractAddr" "$msg" \
-  --from $otherKey --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl"  -b block -o json
-msg=$(cat << EOF
-{"execute": {"proposal_id": $proposal_id}}
-EOF
-)
+  --from $otherKey1 --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json
+
+echo "$otherKey2 votes yes:"
+tgrade tx wasm execute \
+  "$valVotingContractAddr" "$msg" \
+  --from $otherKey2 --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json
 
 # Check if passed
 proposal_status=$(tgrade query wasm contract-state smart $valVotingContractAddr "{\"proposal\": { \"proposal_id\": $proposal_id }}" --node $nodeUrl -o json | jq -er .data.status)
 echo "Proposal $proposal_id status: $proposal_status"
+[ "$proposal_status" != "passed" ] && echo "Proposal $proposal_id not passed" && exit 1
 
+# Execute
+msg=$(cat <<EOF
+{"execute": {"proposal_id": $proposal_id}}
+EOF
+)
 echo "-----------------------"
 echo "Execute proposal: $msg"
 echo "-----------------------"
-tgrade tx wasm execute  \
+tgrade tx wasm execute \
   "$valVotingContractAddr" "$msg" \
-  --from $key --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl"  -b block -o json
+  --from $key --gas auto --gas-prices=0.1utgd --gas-adjustment=1.2 -y --chain-id="$chainId" --node="$nodeUrl" -b block -o json
 
 echo "--------------------------------------------------------------------------------------------"
 echo "## Remember to stop chain at height: $haltHeight, and replace binaries!"
 echo "--------------------------------------------------------------------------------------------"
-echo "Current height: $(tgrade status --node="$nodeUrl" |jq -re ".SyncInfo.latest_block_height" )"
+echo "Current height: $(tgrade status --node="$nodeUrl" | jq -re ".SyncInfo.latest_block_height")"
