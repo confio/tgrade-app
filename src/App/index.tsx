39e9fd67b883ff7c6715bda96150a6e8474078ac
Zhenja <evg.bazhanov@yahoo.com>
2022-03-23 08:18:37 +0100

    draft mobile-view

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,7 @@
 import { config } from "config/network";
 import { i18n } from "i18n/config";
 import { lazy, Suspense } from "react";
+import { isMobile } from "react-device-detect";
 import { I18nextProvider } from "react-i18next";
 import {
   QueryClient as ReactQueryClient,
@@ -23,55 +24,61 @@ const TMarketHome = lazy(() => import("App/pages/TMarket"));
 
 export default function App(): JSX.Element {
   return (
-    <I18nextProvider i18n={i18n}>
-      <ErrorProvider>
-        <ReactQueryClientProvider client={new ReactQueryClient()}>
-          <SdkProvider config={config}>
-            <OcProvider>
-              <Suspense fallback={null}>
-                <ThemeProvider>
-                  <Router basename={process.env.PUBLIC_URL}>
-                    <LayoutProvider>
-                      <Switch>
-                        <Suspense fallback={<LoadingSpinner fullPage />}>
-                          <Route exact path={paths.root}>
-                            <Redirect to={`${paths.dso.prefix}`} />
-                          </Route>
-                          <Route path={`${paths.dso.prefix}${paths.dso.params.dsoAddressOptional}`}>
-                            <DsoProvider>
-                              <Dso />
-                            </DsoProvider>
-                          </Route>
-                          <Route path={paths.engagement.prefix}>
-                            <Engagement />
-                          </Route>
-                          <Route path={paths.validators.prefix}>
-                            <ValidatorsHome />
-                          </Route>
-                          <Route path={paths.oc.prefix}>
-                            <OcHome />
-                          </Route>
-                          <Route path={paths.cpool.prefix}>
-                            <CPoolHome />
-                          </Route>
-                          <Route path={`${paths.tmarket.prefix}`}>
-                            <TMarketProvider>
-                              <TMarketHome />
-                            </TMarketProvider>
-                          </Route>
-                          <Route path={`${paths.documentation.prefix}`}>
-                            <DocumentationPage />
-                          </Route>
-                        </Suspense>
-                      </Switch>
-                    </LayoutProvider>
-                  </Router>
-                </ThemeProvider>
-              </Suspense>
-            </OcProvider>
-          </SdkProvider>
-        </ReactQueryClientProvider>
-      </ErrorProvider>
-    </I18nextProvider>
+    <>
+      {isMobile ? (
+        "How to style message here?"
+      ) : (
+        <I18nextProvider i18n={i18n}>
+          <ErrorProvider>
+            <ReactQueryClientProvider client={new ReactQueryClient()}>
+              <SdkProvider config={config}>
+                <OcProvider>
+                  <Suspense fallback={null}>
+                    <ThemeProvider>
+                      <Router basename={process.env.PUBLIC_URL}>
+                        <LayoutProvider>
+                          <Switch>
+                            <Suspense fallback={<LoadingSpinner fullPage />}>
+                              <Route exact path={paths.root}>
+                                <Redirect to={`${paths.dso.prefix}`} />
+                              </Route>
+                              <Route path={`${paths.dso.prefix}${paths.dso.params.dsoAddressOptional}`}>
+                                <DsoProvider>
+                                  <Dso />
+                                </DsoProvider>
+                              </Route>
+                              <Route path={paths.engagement.prefix}>
+                                <Engagement />
+                              </Route>
+                              <Route path={paths.validators.prefix}>
+                                <ValidatorsHome />
+                              </Route>
+                              <Route path={paths.oc.prefix}>
+                                <OcHome />
+                              </Route>
+                              <Route path={paths.cpool.prefix}>
+                                <CPoolHome />
+                              </Route>
+                              <Route path={`${paths.tmarket.prefix}`}>
+                                <TMarketProvider>
+                                  <TMarketHome />
+                                </TMarketProvider>
+                              </Route>
+                              <Route path={`${paths.documentation.prefix}`}>
+                                <DocumentationPage />
+                              </Route>
+                            </Suspense>
+                          </Switch>
+                        </LayoutProvider>
+                      </Router>
+                    </ThemeProvider>
+                  </Suspense>
+                </OcProvider>
+              </SdkProvider>
+            </ReactQueryClientProvider>
+          </ErrorProvider>
+        </I18nextProvider>
+      )}
+    </>
   );
 }
