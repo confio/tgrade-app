55a9f175ea9ac042f32604848edb26ce7261bfa6
Zhenja <evg.bazhanov@yahoo.com>
2022-04-27 17:43:19 +0200

    attempt #8

--- cypress/steps-definition/T-Market.ts
+++ cypress/steps-definition/T-Market.ts
@@ -111,11 +111,10 @@ And("I enter value for TGN token {string}", (value) => {
 
 And("I enter value for TGN token {string} Exchange tab", (value) => {
   cy.get(tMarketPage.getFromFieldNumber()).type(value).click()
-  cy.wait(10000) //workaround
+  cy.wait(5000) //workaround
 });
 
 And("I see amount of my token {string} Exchange tab", (value) => {
-  cy.wait(10000) //workaround
   cy.get(tMarketPage.getToFieldNumber()).should("have.value", value);
   cy.findByText('Minimum Received:').should("be.visible")
 });
