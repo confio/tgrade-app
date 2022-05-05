f6a5095bf6d534a96034f51c396bf4044ea99ae3
Zhenja <evg.bazhanov@yahoo.com>
2022-05-05 15:32:30 +0200

    improvements

--- src/__tests__/integration/happy-paths/wallet.test.ts
+++ src/__tests__/integration/happy-paths/wallet.test.ts
@@ -4,18 +4,19 @@ import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
 import { makeCosmoshubPath } from "@cosmjs/stargate";
 import { config } from "config/network";
 import { sendTokens } from "utils/currency";
-import { createSigningClient, generateMnemonic, loadOrCreateWallet } from "utils/sdk";
+import { Contract20WS } from "utils/cw20";
+import { createSigningClient, generateMnemonic } from "utils/sdk";
+import { TcContract } from "utils/trustedCircle";
 
-import { Contract20WS } from "../../../utils/cw20";
-import { TcContract } from "../../../utils/trustedCircle";
-
-const mnemonic = generateMnemonic();
+const mnemonic_01 = generateMnemonic();
 const addressPrefix = "tgrade";
 const mnemonic_02 = generateMnemonic();
+const mnemonic_03 = generateMnemonic();
+const mnemonic_04 = generateMnemonic();
 
 describe("Wallet", () => {
   it("Send native token", async () => {
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_01, {
       hdPaths: [makeCosmoshubPath(0)],
       prefix: addressPrefix,
     });
@@ -54,23 +55,21 @@ describe("Wallet", () => {
     expect(parseInt(walletBalanceUserBAfterSend.amount)).toBe(200000);
   }, 10000);
 
-  xit("Send CW20 token", async () => {
+  it("Send CW20 token", async () => {
     const tokenSymbol = "SUST";
     const tokenName = "Sustainability Coin";
     const tokenDecimals = 1;
 
-    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    const wallet = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_01, {
       hdPaths: [makeCosmoshubPath(0)],
       prefix: addressPrefix,
     });
 
-    const wallet_02 = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_02, {
+    const wallet_02 = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_03, {
       hdPaths: [makeCosmoshubPath(0)],
       prefix: config.addressPrefix,
     });
 
-    const signer = await loadOrCreateWallet(config);
-    const address = (await signer.getAccounts())[0].address;
     const signingClient_01 = await createSigningClient(config, wallet);
     const { address: walletUserA } = (await wallet.getAccounts())[0];
 
@@ -82,7 +81,7 @@ describe("Wallet", () => {
 
     // Before Send
     const walletBalanceUserABeforeSend = await signingClient_01.getBalance(walletUserA, config.feeToken);
-    expect(parseInt(walletBalanceUserABeforeSend.amount)).toBe(10000000);
+    expect(parseInt(walletBalanceUserABeforeSend.amount)).toBe(19790000);
 
     const walletBalanceUserBBeforeSend = await signingClient_02.getBalance(walletUserB, config.feeToken);
     expect(parseInt(walletBalanceUserBBeforeSend.amount)).toBe(0);
@@ -99,7 +98,7 @@ describe("Wallet", () => {
       tokenName,
       tokenSymbol,
       tokenDecimals,
-      [{ address, amount }],
+      [{ address: walletUserA, amount }],
       undefined,
       undefined,
       undefined,
@@ -109,13 +108,13 @@ describe("Wallet", () => {
 
     // After Send
     const walletBalanceUserAAfterSend = await signingClient_01.getBalance(walletUserA, config.feeToken);
-    expect(parseInt(walletBalanceUserAAfterSend.amount)).toBe(9790000);
+    expect(parseInt(walletBalanceUserAAfterSend.amount)).toBe(19755000);
 
     const walletBalanceUserBAfterSend = await signingClient_02.getBalance(walletUserB, config.feeToken);
-    expect(parseInt(walletBalanceUserBAfterSend.amount)).toBe(200000);
+    expect(parseInt(walletBalanceUserBAfterSend.amount)).toBe(0);
   }, 15000);
 
-  xit("Send Trusted token", async () => {
+  it("Send Trusted token", async () => {
     const tcName = "Trusted Circle #1";
     const escrowAmount = "1000000";
     const votingPeriod = "19";
@@ -126,18 +125,16 @@ describe("Wallet", () => {
     const tokenName = "Sustainability Coin";
     const tokenDecimals = 1;
 
-    const wallet_01 = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic, {
+    const wallet_01 = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_01, {
       hdPaths: [makeCosmoshubPath(0)],
       prefix: addressPrefix,
     });
 
-    const wallet_02 = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_02, {
+    const wallet_02 = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_04, {
       hdPaths: [makeCosmoshubPath(0)],
       prefix: config.addressPrefix,
     });
 
-    const signer = await loadOrCreateWallet(config);
-    const address = (await signer.getAccounts())[0].address;
     const signingClient_01 = await createSigningClient(config, wallet_01);
     const { address: walletUserA } = (await wallet_01.getAccounts())[0];
 
@@ -149,7 +146,7 @@ describe("Wallet", () => {
 
     // Before Send
     const walletBalanceUserABeforeSend = await signingClient_01.getBalance(walletUserA, config.feeToken);
-    expect(parseInt(walletBalanceUserABeforeSend.amount)).toBe(10000000);
+    expect(parseInt(walletBalanceUserABeforeSend.amount)).toBe(29755000);
 
     const walletBalanceUserBBeforeSend = await signingClient_02.getBalance(walletUserB, config.feeToken);
     expect(parseInt(walletBalanceUserBBeforeSend.amount)).toBe(0);
@@ -160,7 +157,7 @@ describe("Wallet", () => {
     const tcContractAddress = await TcContract.createTc(
       signingClient_01,
       config.codeIds?.tgradeDso?.[0] ?? 0,
-      address,
+      walletUserA,
       tcName,
       escrowAmount,
       votingPeriod,
@@ -177,7 +174,7 @@ describe("Wallet", () => {
       config.gasPrice,
     );
 
-    const amount = Decimal.fromUserInput("11", 6).atomics;
+    const amount = Decimal.fromUserInput("3", 1).atomics;
 
     // Create digital asset
     const cw20tokenAddress = await Contract20WS.createContract(
@@ -187,7 +184,7 @@ describe("Wallet", () => {
       tokenName,
       tokenSymbol,
       tokenDecimals,
-      [{ address, amount }],
+      [{ address: walletUserA, amount }],
       undefined,
       undefined,
       tcContractAddress,
@@ -197,9 +194,9 @@ describe("Wallet", () => {
 
     // After Send
     const walletBalanceUserAAfterSend = await signingClient_01.getBalance(walletUserA, config.feeToken);
-    expect(parseInt(walletBalanceUserAAfterSend.amount)).toBe(9790000);
+    expect(parseInt(walletBalanceUserAAfterSend.amount)).toBe(28695000);
 
     const walletBalanceUserBAfterSend = await signingClient_02.getBalance(walletUserB, config.feeToken);
-    expect(parseInt(walletBalanceUserBAfterSend.amount)).toBe(200000);
-  });
+    expect(parseInt(walletBalanceUserBAfterSend.amount)).toBe(0);
+  }, 20000);
 });
