df0ee7fd8271451f19d0337c32e4732502358a6c
ebazhanov <evg.bazhanov@yahoo.com>
2022-06-12 22:29:41 +0200

    next attempt#2

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.*",
-    excludeSpecPattern: "**/page-object/*,**/steps-definition/*",
+    // We've imported your old e2e plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./e2e/plugins/index.js")(on, config);
+    },
+    specPattern: "./e2e/features/*.feature",
+    excludeSpecPattern: "**/*.{js,ts}",
+    supportFile: false,
+    video: false,
   },
 });
