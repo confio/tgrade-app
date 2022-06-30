94322b4cf843c1546a8940a264d512f74c991e6a
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-06-30 17:18:01 +0200

    Copied ApCreateComplaintModal

---
+++ src/App/components/ApCreateComplaintModal/components/ShowTxResultProposal/index.tsx
@@ -0,0 +1,28 @@
+import Button from "App/components/Button";
+import ShowTxResult, { TxResult } from "App/components/ShowTxResult";
+
+interface ShowTxResultProposalProps {
+  readonly txResult: TxResult;
+  readonly setTxResult: React.Dispatch<React.SetStateAction<TxResult | undefined>>;
+  readonly tryAgain: () => void;
+  readonly resetModal: () => void;
+}
+
+export default function ShowTxResultProposal({
+  txResult,
+  tryAgain,
+  resetModal,
+}: ShowTxResultProposalProps): JSX.Element {
+  return (
+    <ShowTxResult {...txResult}>
+      {txResult.error ? (
+        <Button onClick={() => tryAgain()}>
+          <span>Try again</span>
+        </Button>
+      ) : null}
+      <Button onClick={() => resetModal()}>
+        <span>Go to Arbiter Pool details</span>
+      </Button>
+    </ShowTxResult>
+  );
+}
