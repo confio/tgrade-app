6c41a3beeb1e1628aef3630b431c9ad8a5f5c22f
Zhenja <evg.bazhanov@yahoo.com>
2022-07-15 22:29:53 +0200

    part#2

--- cypress/integration/withdrawMyRewardsToAnother.feature
+++ cypress/integration/withdrawMyRewardsToAnother.feature
@@ -1,6 +1,6 @@
 Feature: Withdraw my rewards to another address
   Background:
-    * I connect to Web Demo wallet
+    * I connect Web Demo wallet
     * Set wallet with Engagement Points and Engagement Rewards
     * Open wallet dialog
     * I see my TGD balance "998"
@@ -10,7 +10,8 @@ Feature: Withdraw my rewards to another address
   Scenario: Withdraw rewards to another
     * I see the "Address" field prefilled with my "tgrade1kalzk5cvq5yu6f5u73k7r905yw52sawckddsc3"
     * I see my Engagement Points "1000 / 2034 (49.16%)" and Engagement Rewards "9"
-    #* I enter the address of the other account on the "Receiver address" field
-    #* I click on the "Withdraw rewards" button
-    #* I see Tx success screen
-    #* I check that the other account's TGD balance has gone up
+    * I enter the address "tgrade1tsg4wldpwyehhkqx3za78ygkzatncxxup96k7h" of the other account on the "Receiver address" field
+    * I click on the "Withdraw rewards" button
+    * I see Tx success screen with my address "tgrade1tsg4wldpwyehhkqx3za78ygkzatncxxup96k7h"
+    * I click Go to Engagement button
+    * I see my Engagement Points "1000 / 2034 (49.16%)" and Engagement Rewards "0 TGD"
--- cypress/steps-definition/Engagement.ts
+++ cypress/steps-definition/Engagement.ts
@@ -10,8 +10,8 @@ And('I see the "Address" field prefilled with my {string}', (walletAddress) => {
   cy.get(engagementPage.getQueryAddressInputField()).should("have.value", walletAddress);
 });
 
-And('I enter the address of the other account on the "Receiver address" field', (walletAddress) => {
-  cy.get(engagementPage.getReceiverAddressInputField()).should("have.value", walletAddress);
+And('I enter the address {string} of the other account on the "Receiver address" field', (address) => {
+  cy.get(engagementPage.getReceiverAddressInputField()).type(address);
 });
 
 And(
@@ -41,3 +41,28 @@ And("I click Go to Engagement button", () => {
 And("I check that my TGD balance has gone up {string}", (tokenBalance) => {
   cy.get(connectWalletModal.getTokenBalance()).should("contain.text", tokenBalance);
 });
+
+/*
+async function createWalletAddress() {
+  const mnemonic_01 = generateMnemonic();
+  const wallet_01 = await DirectSecp256k1HdWallet.fromMnemonic(mnemonic_01, {
+    hdPaths: [makeCosmoshubPath(0)],
+    prefix: "tgrade",
+  });
+
+  const { address: walletUserA } = (await wallet_01.getAccounts())[0];
+
+  //const signingClient_01 = await createSigningClient(config, wallet_01);
+  /!*  const { address: walletUserA } = (await wallet_01.getAccounts())[0];
+
+  const faucetClient = new FaucetClient(config.faucetUrl);
+  await faucetClient.credit(walletUserA, config.faucetTokens?.[0] ?? config.feeToken);
+
+  // User_A wallet before send
+  const walletBalanceUserABeforeSend = await signingClient_01.getBalance(walletUserA, config.feeToken);*!/
+
+  return walletUserA;
+}
+
+const generateMnemonic = (): string => Bip39.encode(Random.getBytes(16)).toString();
+*/
