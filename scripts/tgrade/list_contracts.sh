c9409107404b6083343be74f07a0a0d52fdb24d2
Mauro Lacy <mauro@confio.gmbh>
2022-07-27 17:35:32 +0200

    Add option to list only contract instances

--- scripts/tgrade/list_contracts.sh
+++ scripts/tgrade/list_contracts.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
+set -o errexit -o pipefail
+
+O="$1"
+if [ "$O" = "-h" ] || [ "$O" = "--help" ] || { [ "$O" != "" ] && [ "$O" != "-i" ] && [ "$O" != "--instances" ] ; }
+then
+  echo "Usage: $0 [-h|--help] [-i|--instances]"
+  echo "-h|--help: This cruft."
+  echo "-i|--instances: List only contract instances."
+  echo "Bail as soon as the first one fails."
+  exit 1
+fi
 
 C=1
 while true
@@ -9,6 +19,10 @@ do
   if [ "$A" != "null" ]
   then
     tgrade q wasm contract $A -o json --node="$nodeUrl" 2>/dev/null| jq -re '.'
+  elif [ "$O" = "-i" ] || [ "$O" = "--instances" ]
+  then
+    # Download wasm and get contract name and version from it
+    tgrade q wasm code "$C" /dev/null --node=$nodeUrl >/dev/null 2>&1
   else
     # Download wasm and get contract name and version from it
     tgrade q wasm code "$C" "./contracts/$C.wasm" --node=$nodeUrl 2>/dev/null
