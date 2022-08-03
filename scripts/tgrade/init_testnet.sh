3e0c581244d606b752fa215a9d6330289ef31629
Abel Fernández <abel.fernandez.nandin@gmail.com>
2022-08-03 13:51:40 +0200

    Add invalid url for safety

--- scripts/tgrade/init_testnet.sh
+++ scripts/tgrade/init_testnet.sh
@@ -7,6 +7,6 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 # Deploy contracts
 "$SCRIPT_DIR/deploy_contracts.mjs" network
 # Query and load validator voting contract's address
-VALIDATOR_VOTING_ADDRESS="$(curl https://lcd.dryrunnet.tgrade.confio.run/tgrade/poe/v1beta1/contract/VALIDATOR_VOTING | jq -r '.address')"
+VALIDATOR_VOTING_ADDRESS="$(curl https://[enter-api-url]/tgrade/poe/v1beta1/contract/VALIDATOR_VOTING | jq -r '.address')"
 # Instantiate factory with validator voting contract's address as migrator
 VALIDATOR_VOTING_ADDRESS=$VALIDATOR_VOTING_ADDRESS "$SCRIPT_DIR/instantiate_factory.mjs" network
