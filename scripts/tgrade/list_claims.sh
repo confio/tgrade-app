e04f9ad44ab75eb835e9507815231c5570661b29
Mauro Lacy <mauro@lacy.com.es>
2022-12-22 20:20:26 +0100

    Add support for historical claims query
    Requires archiving node url

--- scripts/tgrade/list_claims.sh
+++ scripts/tgrade/list_claims.sh
@@ -3,6 +3,14 @@ set -o errexit -o nounset -o pipefail
 #set -x
 
 #source ./env.sh
+H="$1"
+[ "$H" = "-h" ] || [ "$H" = "--help" ] && echo "Usage: $0 [height]" && echo "Specifying height requires archiving node." && exit 1
+if [ -n "$H" ]
+then
+  [ -z "$archUrl" ] && echo "archUrl env var not set!" && exit 1
+  nodeUrl="$archUrl"
+  H="--height=$H"
+fi
 
 stakeAddr=$(tgrade q poe contract-address STAKING -o json --node="$nodeUrl" | jq -re ".address")
 start_after=null
@@ -14,7 +22,7 @@ do
     for member in $members
     do
       echo $member:
-      tgrade query wasm contract-state smart $stakeAddr "{ \"claims\": { \"address\": $member } }" -o json --node="$nodeUrl" | jq '.'
+      tgrade query wasm contract-state smart $stakeAddr "{ \"claims\": { \"address\": $member } }" -o json --node="$nodeUrl" "$H" | jq '.'
     done
     start_after=$(echo $members | jq . 2>/dev/null | tail -1)
   else
