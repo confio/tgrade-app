97f163bfc3c6fae9f279e04096f7376cef69bd7d
Mauro Lacy <mauro@lacy.com.es>
2023-01-05 09:54:33 +0100

    Add a max code id option

--- scripts/tgrade/list_contracts.sh
+++ scripts/tgrade/list_contracts.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
 set -o errexit -o pipefail
 
-O="$1"
-if [ "$O" = "-h" ] || [ "$O" = "--help" ] || { [ "$O" != "" ] && [ "$O" != "-i" ] && [ "$O" != "--instances" ] ; }
-then
-  echo "Usage: $0 [-h|--help] [-i|--instances]"
-  echo "-h|--help: This cruft."
-  echo "-i|--instances: List only contract instances."
-  echo "Bail as soon as the first one fails."
-  exit 1
-fi
+function print_usage() {
+  echo "Usage: $0 [-h|--help] [-i|--instances] [-m|--max <max_code_id>]
+-h|--help: This cruft.
+-i|--instances: List only contract instances.
+-m|--max <code_id>: List only up to <code_id>.
+Bail as soon as the first one fails, or max_code_id is reached."
+}
+
+function remove_opt() {
+    ORIGINAL_OPTS=$(echo "$ORIGINAL_OPTS" | sed "s/\\B$1\\b//")
+}
+
+while [ -n "$1" ]
+do
+case $1 in
+  -h|--help)
+    print_usage
+    exit 1
+    ;;
+  -i|--instances)
+    O="$1"
+    shift
+    ;;
+  -m|--max)
+    shift
+    MAX="$1"
+    shift
+    ;;
+  *)
+    print_usage
+    exit 1
+    ;;
+esac
+done
 
 C=1
 while true
@@ -30,6 +55,10 @@ do
   else
     # Download wasm and get contract name and version from it
     ./get_contract_version.sh $C || exit 0
+  fi
+  if [ -n "$MAX" ] && [ $C -ge $MAX ]
+  then
+    exit 0
   fi
   C=$[C + 1]
 done
