658dd62a97d18752e099686b2e3c2aa8bc00c05c
Mauro Lacy <mauro@lacy.com.es>
2023-01-28 13:26:21 +0100

    List mixer and distribution points

---
+++ scripts/tgrade/list_mixer_points.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+#set -x
+
+#source ./env.sh
+
+mixerAddr=$(tgrade q poe contract-address MIXER -o json --node="$nodeUrl" | jq -re ".address")
+start_after=null
+echo "member,points"
+while [ -n "$start_after" ]
+do
+  members=$(tgrade query wasm contract-state smart $mixerAddr "{ \"list_members_by_points\": { \"limit\": 30, \"start_after\": $start_after } }" -o json --node="$nodeUrl" | jq .data.members)
+  addrs=$(echo $members | jq -r .[].addr)
+  points=$(echo $members | jq .[].points)
+  if [ -n "$addrs" ]
+  then
+    i=1
+    for addr in $addrs
+    do
+      point=$(echo $points | cut -d\  -f$i)
+      echo $addr,$point
+      i=$((i + 1))
+    done
+    start_after="$(echo $members | jq .[-1] 2>/dev/null)" 
+  else
+    start_after=''
+  fi
+done
