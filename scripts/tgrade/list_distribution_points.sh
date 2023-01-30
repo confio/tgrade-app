4fcca40e42b714429f22d13242917edb648b4dc3
Mauro Lacy <mauro@lacy.com.es>
2023-01-30 10:38:59 +0100

    Add distribution points historical query

--- scripts/tgrade/list_distribution_points.sh
+++ scripts/tgrade/list_distribution_points.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-set -o errexit -o nounset -o pipefail
+#set -o errexit -o nounset -o pipefail
+set -o errexit -o pipefail
 #set -x
+
+H="$1"
+[ "$H" = "-h" ] || [ "$H" = "--help" ] && echo "Usage: $0 [height]" && echo "Specifying height requires archiving node." && exit 1
+if [ -n "$H" ]
+then
+  [ -z "$archUrl" ] && echo "archUrl env var not set!" && exit 1
+  nodeUrl="$archUrl"
+  H="--height=$H"
+fi
 
 #source ./env.sh
 
@@ -9,7 +19,7 @@ start_after=null
 echo "member,points"
 while [ -n "$start_after" ]
 do
-  members=$(tgrade query wasm contract-state smart $distrAddr "{ \"list_members_by_points\": { \"limit\": 30, \"start_after\": $start_after } }" -o json --node="$nodeUrl" | jq .data.members)
+  members=$(tgrade query wasm contract-state smart $distrAddr "{ \"list_members_by_points\": { \"limit\": 30, \"start_after\": $start_after } }" $H -o json --node="$nodeUrl" | jq .data.members)
   addrs=$(echo $members | jq -r .[].addr)
   points=$(echo $members | jq .[].points)
   if [ -n "$addrs" ]
